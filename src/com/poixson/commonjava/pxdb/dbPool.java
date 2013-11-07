d3c9523b4547c18cfa61ff8eb69f9820419a8aa8
Pete Lorenzo <lorenzo@poixson.com>
2013-11-07 16:31:46 -0500

    finished dbPool class and added new dbPoolSize class to handle warning messages

--- src/com/poixson/commonjava/pxdb/dbPool.java
+++ src/com/poixson/commonjava/pxdb/dbPool.java
@@ -1,9 +1,13 @@
 package com.poixson.commonjava.pxdb;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import com.poixson.commonjava.Utils.CoolDown;
+import com.poixson.commonjava.Utils.utilsThread;
 
 
 public class dbPool {
@@ -17,28 +21,111 @@ public class dbPool {
 
 	// workers/connections
 	private final List<dbWorker> workers = new ArrayList<dbWorker>();
+	// hard/soft pool size limits
+	private final dbPoolSize poolSize;
 
 
 	protected dbPool(dbConfig config) {
+		if(config == null) throw new NullPointerException("config object cannot be null");
 		this.config = config;
+		poolSize = new dbPoolSize(this);
 		// force first connect
 		getWorker().release();
 	}
 
 
+	// pool size
+	public int getWorkerCount() {
+		return workers.size();
+	}
+
+
+	// get db key
+	public String getKey() {
+		//if(config == null)
+		//	return null;
+		if(config == null) throw new NullPointerException("config object cannot be null");
+		return config.getKey();
+	}
+
+
+	// pool is connected
+	public boolean isConnected() {
+		// TODO: this get a lock and releases. can probably be improved using isLocked()
+		dbWorker worker = getExisting();
+		if(worker == null)
+			return false;
+		worker.release();
+		return true;
+	}
+	// ping the db server
+	public boolean isConnectionValid() {
+		dbWorker worker = getExisting();
+		if(worker == null)
+			return false;
+		try {
+			return worker.getConnection().isValid(1);
+		} catch (SQLException ignore) {
+		} finally {
+			worker.release();
+		}
+		return false;
+	}
+
+
 	// get unused worker
 	public dbWorker getWorker() {
+		dbWorker worker = null;
+		synchronized(workers) {
+			CoolDown maxHardBlocking = CoolDown.get("5s");
+			maxHardBlocking.runAgain();
+			int count = 0;
+			while(true) {
+				count = getWorkerCount();
+				// get existing connection
+				if(count > 0) {
+					worker = getExisting();
+					if(worker != null)
+						break;
+					count = getWorkerCount();
+				}
+				// soft max
+				if(count >= poolSize.getSoft())
+					poolSize.StartWarningThread();
+				// hard max
+				if(count >= poolSize.getHard()) {
+					poolSize.HardLimitWarningMessage();
+					// stop waiting
+					if(maxHardBlocking.runAgain()) {
+						System.out.println("Failed to get a db connection! Blocked for "+maxHardBlocking.getDuration().toLongString()+".. Giving up.");
+						return null;
+					}
+					// wait for a free worker
+					utilsThread.Sleep(100L);
+					continue;
+				}
+				// new worker/connection
+				worker = newWorker();
+				if(worker != null)
+					return worker;
+			}
+		}
+		return worker;
+	}
+	public dbWorker getExisting() {
 		synchronized(workers) {
 			if(workers.isEmpty())
-				return newWorker();
+				return null;
 			// workers in pool
 			Iterator<dbWorker> it = workers.iterator();
 			while(it.hasNext()) {
 				dbWorker worker = it.next();
 				// errored or disconnected
-				if(worker == null || worker.hasError()) {
-					if(worker != null)
+				if(worker == null || worker.hasClosed()) {
+					if(worker != null) {
+						System.out.println("Connection [ "+Integer.toString(worker.getId())+" ] dropped!!");
 						worker.close();
+					}
 					it.remove();
 					continue;
 				}
@@ -46,36 +133,39 @@ public class dbPool {
 				if(worker.inUse())
 					continue;
 				// get lock
-				if(worker.getLock())
+				if(worker.getLock()) {
+//					if(debug)
+//						log.debug("db", "Connection pool size: "+Integer.toString(pool.size()));
 					return worker;
+				}
+else
+System.out.println("FAILED TO GET LOCK!!!!");
 			}
-			// new worker/connection
-			return newWorker();
+			return null;
 		}
 	}
 	// new worker/connection
+	private final CoolDown coolFail = CoolDown.get("2s");
 	private dbWorker newWorker() {
+		// hard limit reached
+		if(getWorkerCount() >= poolSize.getHard())
+			return null;
+		// connect to db
 		Connection conn = config.getConnection();
 		// failed to connect
 		if(conn == null) {
-			//TODO: log.severe("Failed to connect to database! "+config.getKey());
+			if(coolFail.runAgain())
+				System.out.println("Failed to connect to database! "+config.getKey());
 			return null;
 		}
 		// successful connection
 		dbWorker worker = new dbWorker(conn);
 		synchronized(workers) {
 			workers.add(worker);
+			if(worker.getLock())
+				return worker;
 		}
-		return worker;
-	}
-
-
-	public boolean isConnected() {
-		dbWorker worker = getWorker();
-		if(worker == null)
-			return false;
-		worker.release();
-		return true;
+		return null;
 	}
 
 
---
+++ src/com/poixson/commonjava/pxdb/dbPoolSize.java
@@ -0,0 +1,125 @@
+package com.poixson.commonjava.pxdb;
+
+import com.poixson.commonjava.Utils.CoolDown;
+import com.poixson.commonjava.Utils.utilsMath;
+import com.poixson.commonjava.Utils.utilsThread;
+
+
+public class dbPoolSize extends Thread {
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException();
+	}
+
+	// max connections
+	private volatile int SOFT = 5;
+	private volatile int HARD = 10;
+
+	private final dbPool pool;
+
+	private volatile boolean running = false;
+	private final Object lock = new Object();
+
+
+	// hard/soft pool size limits
+	protected dbPoolSize(dbPool pool) {
+		if(pool == null) throw new NullPointerException("pool cannot be null");
+		this.pool = pool;
+		this.setName(pool.getKey()+" Warning Thread");
+	}
+
+
+	// pool size warnings
+	public void StartWarningThread() {
+		synchronized(lock) {
+			if(running) return;
+			running = true;
+//			this.setName(pool.getKey()+" Warning Thread");
+			if(!this.isAlive())
+				this.start();
+		}
+//switch(thread.getState()) {
+//case NEW:System.out.println("NEW");break;case RUNNABLE:System.out.println("RUNNABLE");break;case BLOCKED:System.out.println("BLOCKED");break;
+//case WAITING:System.out.println("WAITING");break;case TIMED_WAITING:System.out.println("TIMED_WAITING");break;case TERMINATED:System.out.println("TERMINATED");break;}
+//System.out.println("STARTIING THREAD");
+	}
+	@Override
+	public void run() {
+		System.out.println("Started warning thread.. "+this.getName());
+		running = true;
+//		synchronized(thread) {
+//			if(running) return;
+//			running = true;
+//		}
+		coolSoftLimit.Reset();
+		coolHardLimit.Reset();
+		int count = getWorkerCount();
+		while(count > SOFT) {
+			// try to close unused
+			dbWorker worker = pool.getExisting();
+			// check again after dropping closed workers
+			count = getWorkerCount();
+			if(count <= SOFT) {
+				if(worker != null)
+					worker.release();
+				break;
+			}
+			// try closing a worker
+			if(worker != null)
+				worker.close();
+			// warning message
+			if(count >= HARD)
+				HardLimitWarningMessage();
+			else
+			if(count > SOFT)
+				SoftLimitWarningMessage();
+			// sleep thread
+			utilsThread.Sleep(250L);
+		}
+		System.out.println("Stopped warning thread. "+this.getName());
+		running = false;
+	}
+
+
+	// warning messages (with cooldown)
+	private final CoolDown coolSoftLimit = CoolDown.get("10s");
+	protected void SoftLimitWarningMessage() {
+		int count = getWorkerCount();
+		if(count <= SOFT) return;
+		// don't spam/flood console
+		if(coolSoftLimit.runAgain())
+			System.out.println("DB connection pool nearing limit! [ "+Integer.toString(count)+" max: "+Integer.toString(HARD)+" ] "+pool.getKey());
+	}
+	private final CoolDown coolHardLimit = CoolDown.get("2s");
+	protected void HardLimitWarningMessage() {
+		int count = getWorkerCount();
+		if(count < HARD) return;
+		// don't spam/flood console
+		if(coolHardLimit.runAgain())
+			System.out.println("DB connection pool HARD LIMIT REACHED!! [ "+Integer.toString(count)+" max: "+Integer.toString(HARD)+" ] "+pool.getKey());
+	}
+
+
+	// set hard/soft limits
+	public void setSoft(int limit) {
+		this.SOFT = utilsMath.MinMax(limit, 1, 1000);
+	}
+	public void setHard(int limit) {
+		this.HARD = utilsMath.MinMax(limit, 1, 1000);
+	}
+	//get hard/soft limits
+	public int getSoft() {
+		return SOFT;
+	}
+	public int getHard() {
+		return HARD;
+	}
+
+
+	// db connections count
+	public int getWorkerCount() {
+		return pool.getWorkerCount();
+	}
+
+
+}
