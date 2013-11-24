a5a9f34809dff1dc78976f130cb0830e3d5f0ff5
Pete Lorenzo <lorenzo@poixson.com>
2013-11-23 20:52:31 -0500

    added sqlIdField and sqlField() to FieldDAO class to generate sql

--- src/com/poixson/commonjava/pxdb/TableManager/FieldDAO.java
+++ src/com/poixson/commonjava/pxdb/TableManager/FieldDAO.java
@@ -1,6 +1,8 @@
 package com.poixson.commonjava.pxdb.TableManager;
 
 import com.poixson.commonjava.Utils.utilsSan;
+import com.poixson.commonjava.pxdb.dbQuery;
+
 
 public class FieldDAO {
 
@@ -21,6 +23,63 @@ public class FieldDAO {
 	}
 
 
+	// id field
+	public static String sqlIdField(String fieldName) {
+		return (new StringBuilder())
+			.append("`")
+			.append(dbQuery.san(fieldName))
+			.append("` int(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY (`")
+			.append(dbQuery.san(fieldName))
+			.append("`)")
+		.toString();
+	}
+	// `name` type(size) NULL DEFAULT NULL
+	public String sqlField() {
+		StringBuilder sql = new StringBuilder();
+		sql.append("`").append(fieldName).append("` ");
+		switch(type.toLowerCase()) {
+		case "s":
+		case "str":
+		case "string":
+			sql.append("VARCHAR").append("(").append(size).append(")");
+			break;
+		case "i":
+		case "int":
+		case "integer":
+			sql.append("INT").append("(").append(size).append(")");
+			break;
+		case "dec":
+		case "decimal":
+			sql.append("DECIMAL").append("(").append(size).append(")");
+			break;
+		case "d":
+		case "dbl":
+		case "double":
+			sql.append("DOUBLE").append("(").append(size).append(")");
+			break;
+		case "f":
+		case "flt":
+		case "float":
+			sql.append("FLOAT").append("(").append(size).append(")");
+			break;
+		case "l":
+		case "lng":
+		case "long":
+			sql.append("LONG").append("(").append(size).append(")");
+			break;
+		case "b":
+		case "bool":
+		case "boolean":
+			sql.append("TINYINT(1)");
+			break;
+		case "t":
+		case "txt":
+		case "text":
+			sql.append("TEXT");
+		default:
+			break;
+		}
+		return sql.toString();
 	}
 
 
