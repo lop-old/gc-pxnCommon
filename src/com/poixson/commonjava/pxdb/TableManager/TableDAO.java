1f6b948f39d2320bf55f3d52df5973ac7b7155b3
Pete Lorenzo <lorenzo@poixson.com>
2013-11-23 20:59:01 -0500

    added sqlCreateTable() sqlAddField() and sqlFields() to TableDAO class to generate sql

--- src/com/poixson/commonjava/pxdb/TableManager/TableDAO.java
+++ src/com/poixson/commonjava/pxdb/TableManager/TableDAO.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.poixson.commonjava.Utils.utilsSan;
+import com.poixson.commonjava.pxdb.dbQuery;
+
 
 public class TableDAO {
 
 	public final String tableName;
+
+	public volatile String idField = "";
 
 	public final List<FieldDAO> fields = new ArrayList<FieldDAO>();
 	public final List<String>   unique = new ArrayList<String>();
@@ -43,6 +47,45 @@ public class TableDAO {
 	public TableDAO unique(String fieldName) {
 		this.unique.add(fieldName);
 		return this;
+	}
+
+
+	// CREATE TABLE name ( fields ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1
+	public String sqlCreateTable() {
+		return (new StringBuilder())
+			.append("CREATE TABLE `")
+			.append(dbQuery.san(this.tableName))
+			.append("` ( ")
+			.append(sqlFields())
+			.append(" ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1")
+		.toString();
+	}
+	// ALTER TABLE name ADD field
+	public String sqlAddField(FieldDAO field) {
+		return "ALTER TABLE `"+field.sqlField();
+	}
+	// sql for fields
+	public String sqlFields() {
+		StringBuilder sql = new StringBuilder();
+		// id field
+		if(getIdField() != null) {
+			sql.append(
+				FieldDAO.sqlIdField(getIdField())
+			);
+		}
+		// all other fields
+		for(FieldDAO field : this.fields) {
+			if(sql.length() > 0)
+				sql.append(", ");
+			sql.append(field.sqlField());
+		}
+		// unique fields
+		for(String u : this.unique) {
+			if(sql.length() > 0)
+				sql.append(", ");
+			sql.append("UNIQUE KEY `"+u+"` (`"+u+"`)");
+		}
+		return sql.toString();
 	}
 
 
