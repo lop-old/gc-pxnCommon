bc8986fdf0a9eb4069e6a4fc1d5dd6f637999769
Pete Lorenzo <lorenzo@poixson.com>
2013-12-10 14:17:00 -0500

    cleaned up xLogPrinting class a bit and removed boolean publishing

--- src/com/poixson/commonjava/xLogger/xLogPrinting.java
+++ src/com/poixson/commonjava/xLogger/xLogPrinting.java
@@ -41,89 +41,35 @@ public abstract class xLogPrinting {
 
 	// finest
 	public void finest(String msg) {
-		publish(
-			xLevel.FINEST,
-			msg
-		);
+		publish(xLevel.FINEST, msg);
 	}
 	// finer
 	public void finer(String msg) {
-		publish(
-			xLevel.FINER,
-			msg
-		);
+		publish(xLevel.FINER, msg);
 	}
 	// fine
 	public void fine(String msg) {
-		publish(
-			xLevel.FINE,
-			msg
-		);
+		publish(xLevel.FINE, msg);
 	}
 	// stats
 	public void stats(String msg) {
-		publish(
-			xLevel.STATS,
-			msg
-		);
+		publish(xLevel.STATS, msg);
 	}
 	// info
 	public void info(String msg) {
-		publish(
-			xLevel.INFO,
-			msg
-		);
+		publish(xLevel.INFO, msg);
 	}
 	// warning
 	public void warning(String msg) {
-		publish(
-			xLevel.WARNING,
-			msg
-		);
+		publish(xLevel.WARNING, msg);
 	}
 	// severe
 	public void severe(String msg) {
-		publish(
-			xLevel.SEVERE,
-			msg
-		);
+		publish(xLevel.SEVERE, msg);
 	}
 	// fatal
 	public void fatal(String msg) {
-		publish(
-			xLevel.FATAL,
-			msg
-		);
-	}
-
-
-	// boolean
-	public void finest(Boolean b) {
-		finest(utilsString.toString(b));
-	}
-	public void finer(Boolean b) {
-		finer(utilsString.toString(b));
-	}
-	public void fine(Boolean b) {
-		fine(utilsString.toString(b));
-	}
-	public void debug(Boolean b) {
-		debug(utilsString.toString(b));
-	}
-	public void stats(Boolean b) {
-		stats(utilsString.toString(b));
-	}
-	public void info(Boolean b) {
-		info(utilsString.toString(b));
-	}
-	public void warning(Boolean b) {
-		warning(utilsString.toString(b));
-	}
-	public void severe(Boolean b) {
-		severe(utilsString.toString(b));
-	}
-	public void fatal(Boolean b) {
-		fatal(utilsString.toString(b));
+		publish(xLevel.FATAL, msg);
 	}
 
 
