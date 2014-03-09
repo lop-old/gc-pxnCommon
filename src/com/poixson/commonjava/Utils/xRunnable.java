705d5183077cd9b445dc3187603d3821faac538a
Pete Lorenzo <lorenzo@poixson.com>
2014-03-08 19:55:55 -0500

    added xRunnable class

---
+++ src/com/poixson/commonjava/Utils/xRunnable.java
@@ -0,0 +1,47 @@
+package com.poixson.commonjava.Utils;
+
+
+public class xRunnable implements Runnable {
+
+	protected volatile String taskName = null;
+	protected final Runnable run;
+
+
+	public xRunnable() {
+		run = null;
+	}
+	public xRunnable(final String taskName) {
+		this(taskName, null);
+	}
+	public xRunnable(final Runnable run) {
+		this(null, run);
+	}
+	public xRunnable(final String taskName, final Runnable run) {
+		if(utils.notEmpty(taskName))
+			this.taskName = taskName;
+		else if(run instanceof xRunnable)
+			this.taskName = ((xRunnable) run).getTaskName();
+		this.run = run;
+	}
+
+
+	public static xRunnable cast(final Runnable run) {
+		if(run instanceof xRunnable)
+			return (xRunnable) run;
+		return new xRunnable(run);
+	}
+
+
+	@Override
+	public void run() {
+		if(this.run == null) throw new NullPointerException();
+		this.run.run();
+	}
+
+
+	public String getTaskName() {
+		return this.taskName;
+	}
+
+
+}
