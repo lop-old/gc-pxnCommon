2f7e5c5ba42ce0cfca3f58997648fa91b08733d5
Pete Lorenzo <lorenzo@poixson.com>
2014-03-10 17:15:00 -0400

    cleaned up xClock and ntpMessage classes

--- src/com/poixson/commonjava/Utils/ntpMessage.java
+++ src/com/poixson/commonjava/Utils/ntpMessage.java
@@ -1,4 +1,4 @@
-package com.growcontrol.gcCommon.pxnClock;
+package com.poixson.commonjava.Utils;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -58,8 +58,9 @@ import java.util.Date;
  *
  * @author Adam Buckley
  */
-public class ntpMessage
-{
+
+public class ntpMessage {
+
 	/**
 	 * This is a two-bit code warning of an impending leap second to be
 	 * inserted/deleted in the last minute of the current day.  It's values
@@ -74,7 +75,6 @@ public class ntpMessage
 	 */
 	public byte leapIndicator = 0;
 
-
 	/**
 	 * This value indicates the NTP/SNTP version number.  The version number
 	 * is 3 for Version 3 (IPv4 only) and 4 for Version 4 (IPv4, IPv6 and OSI).
@@ -82,7 +82,6 @@ public class ntpMessage
 	 * encapsulating context must be inspected.
 	 */
 	public byte version = 3;
-
 
 	/**
 	 * This value indicates the mode, with values defined as follows:
@@ -104,7 +103,6 @@ public class ntpMessage
 	 */
 	public byte mode = 0;
 
-
 	/**
 	 * This value indicates the stratum level of the local clock, with values
 	 * defined as follows:
@@ -118,7 +116,6 @@ public class ntpMessage
 	 */
 	public short stratum = 0;
 
-
 	/**
 	 * This value indicates the maximum interval between successive messages,
 	 * in seconds to the nearest power of two. The values that can appear in
@@ -127,7 +124,6 @@ public class ntpMessage
 	 */
 	public byte pollInterval = 0;
 
-
 	/**
 	 * This value indicates the precision of the local clock, in seconds to
 	 * the nearest power of two.  The values that normally appear in this field
@@ -135,7 +131,6 @@ public class ntpMessage
 	 * found in some workstations.
 	 */
 	public byte precision = 0;
-
 
 	/**
 	 * This value indicates the total roundtrip delay to the primary reference
@@ -147,14 +142,12 @@ public class ntpMessage
 	 */
 	public double rootDelay = 0;
 
-
 	/**
 	 * This value indicates the nominal error relative to the primary reference
 	 * source, in seconds.  The values  that normally appear in this field
 	 * range from 0 to several hundred milliseconds.
 	 */
 	public double rootDispersion = 0;
-
 
 	/**
 	 * This is a 4-byte array identifying the particular reference source.
@@ -192,13 +185,11 @@ public class ntpMessage
 	 */
 	public byte[] referenceIdentifier = {0, 0, 0, 0};
 
-
 	/**
 	 * This is the time at which the local clock was last set or corrected, in
 	 * seconds since 00:00 1-Jan-1900.
 	 */
 	public double referenceTimestamp = 0;
-
 
 	/**
 	 * This is the time at which the request departed the client for the
@@ -206,13 +197,11 @@ public class ntpMessage
 	 */
 	public double originateTimestamp = 0;
 
-
 	/**
 	 * This is the time at which the request arrived at the server, in seconds
 	 * since 00:00 1-Jan-1900.
 	 */
 	public double receiveTimestamp = 0;
-
 
 	/**
 	 * This is the time at which the reply departed the server for the client,
@@ -221,12 +210,10 @@ public class ntpMessage
 	public double transmitTimestamp = 0;
 
 
-
 	/**
 	 * Constructs a new NtpMessage from an array of bytes.
 	 */
-	public ntpMessage(byte[] array)
-	{
+	public ntpMessage(final byte[] array) {
 		// See the packet format diagram in RFC 2030 for details
 		leapIndicator = (byte) ((array[0] >> 6) & 0x3);
 		version = (byte) ((array[0] >> 3) & 0x7);
@@ -235,12 +222,14 @@ public class ntpMessage
 		pollInterval = array[2];
 		precision = array[3];
 
-		rootDelay = (array[4] * 256.0) +
+		rootDelay =
+			(array[4] * 256.0) +
 			unsignedByteToShort(array[5]) +
 			(unsignedByteToShort(array[6]) / 256.0) +
 			(unsignedByteToShort(array[7]) / 65536.0);
 
-		rootDispersion = (unsignedByteToShort(array[8]) * 256.0) +
+		rootDispersion =
+			(unsignedByteToShort(array[8]) * 256.0) +
 			unsignedByteToShort(array[9]) +
 			(unsignedByteToShort(array[10]) / 256.0) +
 			(unsignedByteToShort(array[11]) / 65536.0);
@@ -257,26 +246,22 @@ public class ntpMessage
 	}
 
 
-
 	/**
 	 * Constructs a new NtpMessage in client -> server mode, and sets the
 	 * transmit timestamp to the current time.
 	 */
-	public ntpMessage()
-	{
+	public ntpMessage() {
 		// Note that all the other member variables are already set with
 		// appropriate default values.
 		this.mode = 3;
-		this.transmitTimestamp = (System.currentTimeMillis()/1000.0) + 2208988800.0;
+		this.transmitTimestamp = (System.currentTimeMillis() / 1000.0) + 2208988800.0;
 	}
-
 
 
 	/**
 	 * This method constructs the data bytes of a raw NTP packet.
 	 */
-	public byte[] toByteArray()
-	{
+	public byte[] toByteArray() {
 		// All bytes are automatically set to 0
 		byte[] p = new byte[48];
 
@@ -295,8 +280,8 @@ public class ntpMessage
 		// root dispersion is an unsigned 16.16-bit FP, in Java there are no
 		// unsigned primitive types, so we use a long which is 64-bits
 		long ul = (long) (rootDispersion * 65536.0);
-		p[8] = (byte) ((ul >> 24) & 0xFF);
-		p[9] = (byte) ((ul >> 16) & 0xFF);
+		p[8]  = (byte) ((ul >> 24) & 0xFF);
+		p[9]  = (byte) ((ul >> 16) & 0xFF);
 		p[10] = (byte) ((ul >> 8) & 0xFF);
 		p[11] = (byte) (ul & 0xFF);
 
@@ -314,42 +299,39 @@ public class ntpMessage
 	}
 
 
-
 	/**
 	 * Returns a string representation of a NtpMessage
 	 */
-	public String toString()
-	{
-		String precisionStr =
-			new DecimalFormat("0.#E0").format(Math.pow(2, precision));
-
-		return "Leap indicator: " + leapIndicator + "\n" +
-			"Version: " + version + "\n" +
-			"Mode: " + mode + "\n" +
-			"Stratum: " + stratum + "\n" +
-			"Poll: " + pollInterval + "\n" +
-			"Precision: " + precision + " (" + precisionStr + " seconds)\n" +
-			"Root delay: " + new DecimalFormat("0.00").format(rootDelay*1000) + " ms\n" +
-			"Root dispersion: " + new DecimalFormat("0.00").format(rootDispersion*1000) + " ms\n" +
-			"Reference identifier: " + referenceIdentifierToString(referenceIdentifier, stratum, version) + "\n" +
-			"Reference timestamp: " + timestampToString(referenceTimestamp) + "\n" +
-			"Originate timestamp: " + timestampToString(originateTimestamp) + "\n" +
-			"Receive timestamp:   " + timestampToString(receiveTimestamp) + "\n" +
-			"Transmit timestamp:  " + timestampToString(transmitTimestamp);
+	public String toString() {
+		final String precisionStr = new DecimalFormat("0.#E0").format(Math.pow(2, precision));
+		final StringBuilder str = new StringBuilder();
+		str.append("Leap indicator: ").append(leapIndicator).append("\n");
+		str.append("Version: ").append(version).append("\n");
+		str.append("Mode: ").append(mode).append("\n");
+		str.append("Stratum: ").append(stratum).append("\n");
+		str.append("Poll: ").append(pollInterval).append("\n");
+		str.append("Precision: ").append(precision).append(" (").append(precisionStr).append(" seconds)\n");
+		str.append("Root delay: ").append(new DecimalFormat("0.00").format(rootDelay * 1000)).append(" ms\n");
+		str.append("Root dispersion: ").append(new DecimalFormat("0.00").format(rootDispersion*1000)).append(" ms\n");
+		str.append("Reference identifier: ").append(referenceIdentifierToString(referenceIdentifier, stratum, version)).append("\n");
+		str.append("Reference timestamp: ").append(timestampToString(referenceTimestamp)).append("\n");
+		str.append("Originate timestamp: ").append(timestampToString(originateTimestamp)).append("\n");
+		str.append("Receive timestamp:   ").append(timestampToString(receiveTimestamp)).append("\n");
+		str.append("Transmit timestamp:  ").append(timestampToString(transmitTimestamp));
+		return str.toString();
 	}
-
 
 
 	/**
 	 * Converts an unsigned byte to a short.  By default, Java assumes that
 	 * a byte is signed.
 	 */
-	public static short unsignedByteToShort(byte b)
-	{
-		if((b & 0x80)==0x80) return (short) (128 + (b & 0x7f));
-		else return (short) b;
+	public static short unsignedByteToShort(final byte b) {
+		if((b & 0x80) == 0x80)
+			return (short) (128 + (b & 0x7f));
+		else
+			return (short) b;
 	}
-
 
 
 	/**
@@ -357,109 +339,94 @@ public class ntpMessage
 	 * and return it as a double, according to the NTP 64-bit timestamp
 	 * format.
 	 */
-	public static double decodeTimestamp(byte[] array, int pointer)
-	{
+	public static double decodeTimestamp(final byte[] array, final int pointer) {
 		double r = 0.0;
-
 		for(int i=0; i<8; i++)
-		{
-			r += unsignedByteToShort(array[pointer+i]) * Math.pow(2, (3-i)*8);
-		}
-
+			r += unsignedByteToShort(array[pointer + i]) * Math.pow(2, (3 - i) * 8);
 		return r;
 	}
-
 
 
 	/**
 	 * Encodes a timestamp in the specified position in the message
 	 */
-	public static void encodeTimestamp(byte[] array, int pointer, double timestamp)
-	{
+	public static void encodeTimestamp(byte[] array, final int pointer, double timestamp) {
 		// Converts a double into a 64-bit fixed point
-		for(int i=0; i<8; i++)
-		{
+		for(int i=0; i<8; i++) {
 			// 2^24, 2^16, 2^8, .. 2^-32
-			double base = Math.pow(2, (3-i)*8);
-
+			final double base = Math.pow(2, (3 - i) * 8);
 			// Capture byte value
-			array[pointer+i] = (byte) (timestamp / base);
-
+			array[pointer + i] = (byte) (timestamp / base);
 			// Subtract captured value from remaining total
-			timestamp = timestamp - (double) (unsignedByteToShort(array[pointer+i]) * base);
+			timestamp = timestamp - (double) (unsignedByteToShort(array[pointer + i]) * base);
 		}
-
 		// From RFC 2030: It is advisable to fill the non-significant
 		// low order bits of the timestamp with a random, unbiased
 		// bitstring, both to avoid systematic roundoff errors and as
 		// a means of loop detection and replay detection.
-		array[7] = (byte) (Math.random()*255.0);
+		array[7] = (byte) (Math.random() * 255.0);
 	}
-
 
 
 	/**
 	 * Returns a timestamp (number of seconds since 00:00 1-Jan-1900) as a
 	 * formatted date/time string.
 	 */
-	public static String timestampToString(double timestamp)
-	{
-		if(timestamp==0) return "0";
+	public static String timestampToString(final double timestamp) {
+		if(timestamp == 0)
+			return "0";
 
 		// timestamp is relative to 1900, utc is used by Java and is relative
 		// to 1970
-		double utc = timestamp - (2208988800.0);
+		final double utc = timestamp - (2208988800.0);
 
 		// milliseconds
-		long ms = (long) (utc * 1000.0);
+		final long ms = (long) (utc * 1000.0);
 
 		// date/time
-		String date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").format(new Date(ms));
+		final String date = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").format(new Date(ms));
 
 		// fraction
-		double fraction = timestamp - ((long) timestamp);
-		String fractionSting = new DecimalFormat(".000000").format(fraction);
+		final double fraction = timestamp - ((long) timestamp);
+		final String fractionSting = new DecimalFormat(".000000").format(fraction);
 
 		return date + fractionSting;
 	}
-
 
 
 	/**
 	 * Returns a string representation of a reference identifier according
 	 * to the rules set out in RFC 2030.
 	 */
-	public static String referenceIdentifierToString(byte[] ref, short stratum, byte version)
-	{
+	public static String referenceIdentifierToString(final byte[] ref, final short stratum, final byte version) {
 		// From the RFC 2030:
 		// In the case of NTP Version 3 or Version 4 stratum-0 (unspecified)
 		// or stratum-1 (primary) servers, this is a four-character ASCII
 		// string, left justified and zero padded to 32 bits.
-		if(stratum==0 || stratum==1)
-		{
+		if(stratum == 0 || stratum == 1)
 			return new String(ref);
-		}
-
 		// In NTP Version 3 secondary servers, this is the 32-bit IPv4
 		// address of the reference source.
-		else if(version==3)
-		{
-			return unsignedByteToShort(ref[0]) + "." +
-				unsignedByteToShort(ref[1]) + "." +
-				unsignedByteToShort(ref[2]) + "." +
-				unsignedByteToShort(ref[3]);
+		if(version == 3) {
+			final StringBuilder str = new StringBuilder();
+			str.append(unsignedByteToShort(ref[0])).append(".");
+			str.append(unsignedByteToShort(ref[1])).append(".");
+			str.append(unsignedByteToShort(ref[2])).append(".");
+			str.append(unsignedByteToShort(ref[3]));
+			return str.toString();
 		}
-
 		// In NTP Version 4 secondary servers, this is the low order 32 bits
 		// of the latest transmit timestamp of the reference source.
-		else if(version==4)
-		{
-			return "" + ((unsignedByteToShort(ref[0]) / 256.0) +
-				(unsignedByteToShort(ref[1]) / 65536.0) +
-				(unsignedByteToShort(ref[2]) / 16777216.0) +
-				(unsignedByteToShort(ref[3]) / 4294967296.0));
+		if(version == 4) {
+			final StringBuilder str = new StringBuilder();
+			str.append(unsignedByteToShort(ref[0]) / 256.0);
+			str.append(unsignedByteToShort(ref[1]) / 65536.0);
+			str.append(unsignedByteToShort(ref[2]) / 16777216.0);
+			str.append(unsignedByteToShort(ref[3]) / 4294967296.0);
+			return str.toString();
 		}
-
 		return "";
 	}
+
+
 }
--- src/com/poixson/commonjava/Utils/xClock.java
+++ src/com/poixson/commonjava/Utils/xClock.java
@@ -1,229 +1,194 @@
-package com.growcontrol.gcCommon.pxnClock;
+package com.poixson.commonjava.Utils;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketTimeoutException;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.growcontrol.gcCommon.pxnLogger.pxnLog;
-import com.growcontrol.gcCommon.pxnLogger.pxnLogger;
-import com.growcontrol.gcCommon.pxnUtils.pxnUtilsMath;
 
+public class xClock {
 
-public class pxnClock {
-	// instance
-	private static volatile pxnClock clock = null;
+	public static final String DEFAULT_TIMESERVER = "pool.ntp.org";
+	private volatile String timeserver = null;
+	private volatile boolean enableNTP = true;
+
+	private volatile double localOffset = 0.0;
+	private volatile double lastChecked = 0.0;
+
+	// update thread
+	private volatile Thread thread = null;
+	private volatile boolean blocking = true;
+	private volatile Boolean running = false;
+
+	// clock instance
+	private static volatile xClock instance = null;
 	private static final Object lock = new Object();
 
-	protected volatile boolean enableNTP  = true;
-	protected volatile boolean active = false;
-	protected volatile String timeServer = "pool.ntp.org";
 
-	protected volatile double localOffset = 0.0;
-	protected volatile double lastChecked = 0.0; // system time
-
-	// instance lock
-	protected final Object threadLock = new Object();
-	protected Thread updateThread = null;
-
-
-	// get clock
-	public static pxnClock get(boolean blocking) {
-		if(clock == null) {
+	public static xClock get(final boolean blocking) {
+		if(instance == null) {
 			synchronized(lock) {
-				if(clock == null) {
-					clock = new pxnClock();
-					clock.Update(blocking);
-				}
+				if(instance == null)
+					instance = new xClock(blocking);
 			}
 		}
-		return clock;
+		instance.update();
+		return instance;
 	}
-	public static pxnClock getBlocking() {
+	public static xClock get() {
 		return get(true);
 	}
-	public static pxnClock get() {
-		return get(true);
-	}
+
+
 	// new instance
-	protected pxnClock() {}
+	private xClock(final boolean blocking) {
+		this.blocking = blocking;
+	}
 	@Override
 	public Object clone() throws CloneNotSupportedException {
-		throw new CloneNotSupportedException();
+		throw new ClassCastException();
 	}
 
 
-//	// static update
-//	public static double getLocalOffsetNow() {
-//		return getLocalOffsetNow(null);
-//	}
-//	public static double getLocalOffsetNow(String timeServer) {
-//		pxnClock clock = getBlocking();
-//		if(timeServer != null && !timeServer.isEmpty())
-//			clock.setTimeServer(timeServer);
-//		clock.Update(false);
-//		return clock.getLocalOffset();
-//	}
-
-
-	// update from time server
-	public void Update(boolean enabled, boolean threaded) {
-		setNTP(enabled);
-		Update(threaded);
-	}
-	public void Update(String timeServer, boolean threaded) {
-		setTimeServer(timeServer);
-		Update(threaded);
-	}
-	public synchronized void Update(boolean blocking) {
+	public void update() {
 		if(!enableNTP) return;
-		if(blocking) {
-			// run blocking
-			doUpdate();
-		} else {
-			// run threaded
-			if(updateThread == null) {
-				updateThread = new Thread() {
-					@Override
-					public void run() {
-						doUpdate();
-					}
-				};
+		if(running) return;
+		synchronized(running) {
+			if(running) return;
+			running = true;
+			// wait for update
+			if(blocking) {
+				doUpdate();
+			// update threaded
 			} else {
-				// start thread
-				if(!updateThread.isAlive())
-					updateThread.start();
+				if(thread == null)
+					thread = new Thread() {
+						@Override
+						public void run() {
+							doUpdate();
+						}
+					};
+				thread.start();
 			}
 		}
 	}
 
 
-	// run time query
 	protected void doUpdate() {
 		if(!enableNTP) return;
-		synchronized(lock) {
-			if(active) return;
-			active = true;
-		}
-		double time = System.currentTimeMillis();
-		if(lastChecked != 0.0 && ((time-lastChecked)/1000.0) < 60.0) return;
-		lastChecked = time;
-		DatagramSocket socket = null;;
+		if(!running) return;
+		double time = getSystemTime();
+		// checked in last 60 seconds
+		if(lastChecked != 0.0 && ((time - lastChecked) < 60.0)) return;
+		DatagramSocket socket = null;
 		try {
 			socket = new DatagramSocket();
-socket.setSoTimeout(500);
-			InetAddress address = InetAddress.getByName(timeServer);
+			socket.setSoTimeout(500);
+			final InetAddress address = InetAddress.getByName(timeserver);
 			byte[] buf = new ntpMessage().toByteArray();
-			DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 123);
+			final DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 123);
 			ntpMessage.encodeTimestamp(packet.getData(), 40, fromUnixTimestamp());
 			socket.send(packet);
 			socket.receive(packet);
-			ntpMessage msg = new ntpMessage(packet.getData());
-			// calculate local offset
-			time = System.currentTimeMillis();
+			final ntpMessage msg = new ntpMessage(packet.getData());
+			// calculate local offset for correction
+			time = getSystemTime();
 			localOffset = ((msg.receiveTimestamp - msg.originateTimestamp) + (msg.transmitTimestamp - fromUnixTimestamp(time))) / 2.0;
 			// less than 100ms
 			if(localOffset < 0.1 && localOffset > -0.1) {
-				pxnLog.get().debug("System time only off by "+pxnUtilsMath.FormatDecimal("0.000", localOffset)+", not adjusting.");
+				System.out.println("System time only off by "+utilsMath.FormatDecimal("0.000", localOffset)+", ignoring adjustment.");
 				localOffset = 0.0;
 			} else {
-				pxnLogger log = pxnLog.get();
-				log.info("Internal time adjusted by "+(localOffset>0 ? "+" : "-")+pxnUtilsMath.FormatDecimal("0.000", localOffset)+" seconds");
-				log.debug("System time:   "+timestampToString(time/1000.0));
-				log.debug("Adjusted time: "+getString());
+				System.out.println("Internal time adjusted by "+(localOffset>0 ? "+" : "-")+utilsMath.FormatDecimal("0.000", localOffset)+" seconds");
+				System.out.println("System time:   "+timestampToString(time / 1000.0));
+				System.out.println("Internal time: "+getString());
 			}
-			// clean up
-			msg = null;
-			packet = null;
-			address = null;
-		} catch (UnknownHostException | SocketTimeoutException e) {
-			localOffset = 0.0;
-			pxnLog.get().exception(e);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
-			localOffset = 0.0;
-			pxnLog.get().exception(e);
+			e.printStackTrace();
 		} catch (Exception e) {
-			localOffset = 0.0;
-			pxnLog.get().exception(e);
+			e.printStackTrace();
 		} finally {
-			// clean up
 			if(socket != null)
 				socket.close();
 			socket = null;
 		}
-		active = false;
-//double destinationTimestamp = fromUnixTimestamp();
-//double roundTripDelay = (destinationTimestamp-msg.originateTimestamp) - (msg.transmitTimestamp-msg.receiveTimestamp);
-//double localClockOffset = ((msg.receiveTimestamp - msg.originateTimestamp) + (msg.transmitTimestamp - destinationTimestamp)) / 2;
-//GrowControl.log.severe(msg.toString());
-//System.out.println("Dest. timestamp:     " + NtpMessage.timestampToString(destinationTimestamp));
-//System.out.println("Round-trip delay: " + new DecimalFormat("0.00").format(roundTripDelay*1000) + " ms");
-//System.out.println("Local clock offset: " + new DecimalFormat("0.00").format(localClockOffset*1000) + " ms");
-	}
-	// has update run?
-	public boolean hasUpdated() {
-		if(!enableNTP) return true;
-		return (localOffset != 0.0) && (lastChecked != 0.0);
-	}
-	public boolean isUpdating() {
-		return active;
+		lastChecked = time;
+		running = false;
 	}
 
 
-	// convert from 1970ms to 1900s
-	protected static double fromUnixTimestamp(double timestamp) {
-		return (timestamp/1000.0) + 2208988800.0;
-	}
-	protected static double fromUnixTimestamp() {
-		return fromUnixTimestamp(System.currentTimeMillis());
+	public boolean isRunning() {
+		return running;
 	}
 
 
-	// display time formatted: 29-Jun-2012 03:07:04.794
-	public static String timestampToString(double timestamp) {
-		if(timestamp <= 0.0) return "0";
-		return new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").format(new Date( (long)(timestamp*1000.0) )) +
-			new DecimalFormat("0.000").format( timestamp - ((long)timestamp) );
+	public String getTimeServer() {
+		if(utils.isEmpty(timeserver))
+			return DEFAULT_TIMESERVER;
+		return timeserver;
 	}
-
-
-	// get corrected time
-	public double getLocalOffset() {
-		return localOffset;
+	public void setTimeServer(final String host) {
+		timeserver = host;
 	}
-	public double Seconds() {
-		return Millis() / 1000.0;
-	}
-	public long Millis() {
-		return System.currentTimeMillis() + ((long)(localOffset*1000.0));
-	}
-	public String getString() {
-		return timestampToString(Seconds());
-	}
-
-
-	// enable/disable NTP
-	public boolean isEnabled() {
-		return enableNTP;
-	}
-	public void setNTP(boolean enabled) {
-		this.enableNTP = enabled;
-		if(!enabled) {
-			localOffset = 0.0;
-			lastChecked = 0.0;
+	public void setEnabled(final boolean enabled) {
+		synchronized(running) {
+			this.enableNTP = enabled;
+			if(!enabled) {
+				localOffset = 0.0;
+				lastChecked = 0.0;
+			}
 		}
 	}
 
 
-	// set time server host
-	public void setTimeServer(String timeServer) {
-		if(timeServer == null || timeServer.isEmpty()) throw new NullPointerException("timeServer host cannot be null!");
-		this.timeServer = timeServer;
+	/**
+	 * Get current time from system.
+	 * @return
+	 */
+	public static double getSystemTime() {
+		return System.currentTimeMillis();
+	}
+	/**
+	 * Get current time adjusted by NTP.
+	 * @return
+	 */
+	public double getCurrentTime() {
+		return getSystemTime() - localOffset;
+	}
+
+
+	public long millis() {
+		return (long) getCurrentTime();
+	}
+	public double seconds() {
+		return millis() / 1000.0;
+	}
+
+
+	protected static double fromUnixTimestamp(final double timestamp) {
+		return (timestamp / 1000.0) + 2208988800.0;
+	}
+	protected static double fromUnixTimestamp() {
+		return fromUnixTimestamp(getSystemTime());
+	}
+
+
+	public static String timestampToString(final double timestamp) {
+		if(timestamp <= 0.0) return "0";
+		return new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").format(new Date( (long)(timestamp * 1000.0) ))+
+				(new DecimalFormat("0.000").format( timestamp - ((long) timestamp) ));
+	}
+	public String getString() {
+		return timestampToString(seconds());
 	}
 
 
