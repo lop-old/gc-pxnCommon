120b80cf4bc845f67ed031eb7a56a037759b4192
Pete Lorenzo <lorenzo@poixson.com>
2014-05-15 03:02:58 -0400

    added xVars and xAppVars classes

---
+++ src/com/poixson/commonjava/xVars.java
@@ -0,0 +1,48 @@
+package com.poixson.commonjava;
+
+
+public class xVars {
+
+
+	// singleton instance
+	private static volatile xVars instance = null;
+	protected static final Object instanceLock = new Object();
+	public static xVars get() {
+		if(instance == null) {
+			synchronized(instanceLock) {
+				if(instance == null)
+					instance = new xVars();
+			}
+		}
+		return instance;
+	}
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException();
+	}
+
+
+	// new instance of holder
+	protected xVars() {
+		// clone vars
+		if(instance != null) {
+System.out.println("CLONING xVars");
+			this.debug = instance.debug;
+		// new instance
+		} else {
+
+		}
+	}
+
+
+	// debug mode
+	private static final boolean DEFAULT_DEBUG = false;
+	private volatile Boolean debug = null;
+	public boolean debug() {
+		if(this.debug == null)
+			return DEFAULT_DEBUG;
+		return this.debug.booleanValue();
+	}
+
+
+}
