2bcb01a32fe9551e51fedbe9b6751c77caf3ef74
Pete Lorenzo <lorenzo@poixson.com>
2014-05-24 08:13:41 -0400

    added xConfig and xConfigLoader classes

---
+++ src/com/poixson/commonapp/config/xConfig.java
@@ -0,0 +1,119 @@
+package com.poixson.commonapp.config;
+
+import java.util.List;
+import java.util.Map;
+
+import com.poixson.commonjava.Utils.utilsObject;
+
+
+public class xConfig {
+
+	protected final Map<String, Object> data;
+
+
+	public xConfig(final Map<String, Object> data) {
+		if(data == null) throw new NullPointerException();
+		this.data = data;
+	}
+//	@Override
+//	public Object clone() throws CloneNotSupportedException {
+//		throw new CloneNotSupportedException();
+//	}
+
+
+	// get object
+	public Object get(final String path) {
+		if(path == null || path.isEmpty())
+			return null;
+		try {
+			return this.data.get(path);
+		} catch (Exception ignore) {}
+		return null;
+	}
+	// get path
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> getPath(final String path) {
+		try {
+			return (Map<String, Object>) get(path);
+		} catch (Exception ignore) {}
+		return null;
+	}
+	// get string
+	public String getString(final String path) {
+		try {
+			return (String) get(path);
+		} catch (Exception ignore) {}
+		return null;
+	}
+	// get boolean
+	public Boolean getBoolean(final String path) {
+		try {
+			return (Boolean) get(path);
+		} catch (Exception ignore) {}
+		return null;
+	}
+	// get integer
+	public Integer getInteger(final String path) {
+		try {
+			return (Integer) get(path);
+		} catch (Exception ignore) {}
+		return null;
+	}
+	// get long
+	public Long getLong(final String path) {
+		try {
+			return (Long) get(path);
+		} catch (Exception ignore) {}
+		return null;
+	}
+	// get double
+	public Double getDouble(final String path) {
+		try {
+			return (Double) get(path);
+		} catch (Exception ignore) {}
+		return null;
+	}
+	// get float
+	public Float getFloat(final String path) {
+		try {
+			return (Float) get(path);
+		} catch (Exception ignore) {}
+		return null;
+	}
+
+
+	// get list
+	public <T> List<T> getList(final Class<? extends T> clss, final String path) {
+		try {
+			return utilsObject.castList(clss, get(path));
+		} catch (Exception ignore) {}
+		return null;
+	}
+	// get string list
+	public List<String> getStringList(final String path) {
+		try {
+			return getList(String.class, path);
+		} catch (Exception ignore) {}
+		return null;
+	}
+
+
+//	public List<HashMap<String, Object>> getKeyList(String path) {
+//		try {
+//			@SuppressWarnings("unchecked")
+//			List<HashMap<String, Object>> list = (List<HashMap<String, Object>>) get(path);
+//			return list;
+//		} catch (Exception ignore) {}
+//		return null;
+//	}
+//	public List<pxnConfig> getConfigList(String path) {
+//		List<HashMap<String, Object>> map = getKeyList(path);
+//		if(map == null) return null;
+//		List<pxnConfig> list = new ArrayList<pxnConfig>();
+//		for(HashMap<String, Object> d : map)
+//			list.add(new pxnConfig(d));
+//		return list;
+//	}
+
+
+}
---
+++ src/com/poixson/commonapp/config/xConfigLoader.java
@@ -0,0 +1,106 @@
+package com.poixson.commonapp.config;
+
+import java.io.File;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
+import com.poixson.commonjava.Utils.utils;
+import com.poixson.commonjava.Utils.utilsDirFile;
+import com.poixson.commonjava.xLogger.xLog;
+
+
+public final class xConfigLoader {
+
+
+	private xConfigLoader() {}
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException();
+	}
+
+
+	// load generic yml file
+	public static xConfig Load(final String file) {
+		return Load(file, xConfig.class);
+	}
+	public static xConfig Load(final File file) {
+		return Load(file, xConfig.class);
+	}
+
+
+	// load extended xConfig
+	public static xConfig Load(final String file, final Class<? extends xConfig> clss) {
+		if(file == null || file.isEmpty()) throw new NullPointerException();
+		return Load(new File(file), clss);
+	}
+	@SuppressWarnings("resource")
+	public static xConfig Load(final File file, final Class<? extends xConfig> clss) {
+		if(file == null) throw new NullPointerException();
+		if(clss == null) throw new NullPointerException();
+		log().info("Loading config file: "+file.toString());
+		final InputStream in = utilsDirFile.OpenFile(file);
+		if(in == null) return null;
+		return Load(in, clss);
+	}
+
+
+	// load from jar
+	public static xConfig Load(final File jarFile, final String ymlFile) {
+		return Load(jarFile, ymlFile, xConfig.class);
+	}
+	@SuppressWarnings("resource")
+	public static xConfig Load(final File jarFile, final String ymlFile, final Class<? extends xConfig> clss) {
+		if(jarFile == null) throw new NullPointerException();
+		final utilsDirFile.InputJar in = utilsDirFile.OpenJarResource(jarFile, ymlFile);
+		if(in == null) return null;
+		try {
+			return Load(in.fileInput, clss);
+		} finally {
+			utils.safeClose(in);
+		}
+	}
+
+
+	public static xConfig Load(final InputStream in, final Class<? extends xConfig> clss) {
+		if(in == null) throw new NullPointerException();
+		try {
+			final Yaml yml = new Yaml();
+			@SuppressWarnings("unchecked")
+			final Map<String, Object> data = (HashMap<String, Object>) yml.load(in);
+			if(data == null || data.isEmpty())
+				return null;
+			@SuppressWarnings("unchecked")
+			final Constructor<? extends Map<String, Object>> construct =
+				(Constructor<? extends Map<String, Object>>) clss.getDeclaredConstructor(Map.class);
+			return (xConfig) construct.newInstance(data);
+		} catch (InstantiationException e) {
+			log().trace(e);
+		} catch (IllegalAccessException e) {
+			log().trace(e);
+		} catch (IllegalArgumentException e) {
+			log().trace(e);
+		} catch (InvocationTargetException e) {
+			log().trace(e);
+		} catch (NoSuchMethodException e) {
+			log().trace(e);
+		} catch (SecurityException e) {
+			log().trace(e);
+		} finally {
+			utils.safeClose(in);
+		}
+		return null;
+	}
+
+
+	// logger
+	public static xLog log() {
+		return xLog.getRoot();
+	}
+
+
+}
