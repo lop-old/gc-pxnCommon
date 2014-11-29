b71f39ef86df78ed63ba545e254482a00f4833dc
Lop <lop@matt.lophome.com>
2014-11-29 03:08:47 -0500

    added castMap() function to utilsObject

--- src/com/poixson/commonjava/Utils/utilsObject.java
+++ src/com/poixson/commonjava/Utils/utilsObject.java
@@ -2,7 +2,10 @@ package com.poixson.commonjava.Utils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 
 public final class utilsObject {
@@ -48,6 +51,53 @@ public final class utilsObject {
 		if(object == null) return null;
 		try {
 			return castList(clss, (Collection<T>) object);
+		} catch (Exception ignore) {}
+		return null;
+	}
+
+
+
+	/**
+	 * Cast a map object to a typed map.
+	 * @param keyClss
+	 * @param valClss
+	 * @param m
+	 * @return
+	 */
+	public static <K, V> Map<K, V> castMap(final Class<? extends K> keyClss,
+			final Class<? extends V> valClss, final Map<?, ?> m) {
+		if(keyClss == null) throw new NullPointerException("key clss cannot be null");
+		if(valClss == null) throw new NullPointerException("val clss cannot be null");
+		if(m       == null) return null;
+		try {
+			final Map<K, V> result = new HashMap<K, V>(m.size());
+			for(final Entry<?, ?> entry : m.entrySet()) {
+				try {
+					result.put(
+						cast(keyClss, entry.getKey()),
+						cast(valClss, entry.getValue())
+					);
+				} catch (Exception ignore) {}
+			}
+			return result;
+		} catch (Exception ignore) {}
+		return null;
+	}
+	/**
+	 * Cast an object to a typed map.
+	 * @param keyClss
+	 * @param valClss
+	 * @param m
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <K, V> Map<K, V> castMap(final Class<? extends K> keyClss,
+			final Class<? extends V> valClss, final Object object) {
+		if(keyClss == null) throw new NullPointerException("key clss cannot be null");
+		if(valClss == null) throw new NullPointerException("val clss cannot be null");
+		if(object == null) return null;
+		try {
+			return castMap(keyClss, valClss, (Map<K, V>) object);
 		} catch (Exception ignore) {}
 		return null;
 	}
