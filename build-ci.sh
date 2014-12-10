fc6490812063300f4e01525d250eedbe1712ffb3
lorenzop <lorenzo@poixson.com>
2014-12-10 05:24:24 -0500

    updated build-ci.sh script to use new style

--- build-ci.sh
+++ build-ci.sh
@@ -1,15 +1,33 @@
-# (cd ${WORKSPACE}; sh build-ci.sh)
+# sh build-ci.sh  --dl-path=/home/pxn/www/dl/pxnCommon  --yum-path=/home/pxn/www/yum/extras-testing/noarch
 
 
+# load build_utils.sh script
+if [ -e build_utils.sh ]; then
+	source ./build_utils.sh
+elif [ -e /usr/local/bin/pxn/build_utils.sh ]; then
+	source /usr/local/bin/pxn/build_utils.sh
+else
+	wget https://raw.githubusercontent.com/PoiXson/shellscripts/master/pxn/build_utils.sh \
+		|| exit 1
+	source ./build_utils.sh
+fi
 
-echo "Build.."
+
+NAME="pxnCommon"
+[ -z "${WORKSPACE}" ] && WORKSPACE=`pwd`
+rm -vf "${WORKSPACE}/${NAME}"-*.jar
+rm -vf "${WORKSPACE}/${NAME}"-*.noarch.rpm
+
+
+title "Build.."
 ( cd "${WORKSPACE}/" && sh build-mvn.sh --build-number ${BUILD_NUMBER} ) || exit 1
 ( cd "${WORKSPACE}/" && sh build-rpm.sh --build-number ${BUILD_NUMBER} ) || exit 1
 
 
-
-echo "Deploy.."
-rm -fv "/home/pxn/www/yum/extras-testing/noarch/pxnCommon"-*.noarch.rpm
-cp -fv "${WORKSPACE}/pxnCommon"-*.noarch.rpm "/home/pxn/www/yum/extras-testing/noarch/" || exit 1
-
+title "Deploy.."
+cp -fv "${WORKSPACE}/${NAME}"-*.jar        "${DL_PATH}/" || exit 1
+cp -fv "${WORKSPACE}/${NAME}"-*.noarch.rpm "${DL_PATH}/" || exit 1
+latest_version "${DL_PATH}/${NAME}-*.noarch.rpm"         || exit 1
+echo "Latest version: "${LATEST_FILE}
+ln -fs "${LATEST_FILE}" "${YUM_PATH}/${NAME}.noarch.rpm" || exit 1
 
