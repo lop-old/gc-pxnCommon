fbc477e9f558fb20f8beff371ee7c905467da3ee
lorenzop <lorenzo@poixson.com>
2015-01-05 11:26:48 -0500

    added xLogTest class

---
+++ test/com/poixson/commonjava/logger/xLogTest.java
@@ -0,0 +1,41 @@
+package com.poixson.commonjava.logger;
+
+import org.junit.Assert;
+
+import com.poixson.commonjava.Failure;
+import com.poixson.commonjava.xLogger.xLevel;
+import com.poixson.commonjava.xLogger.xLog;
+
+
+public class xLogTest {
+
+	private static volatile xLog log = null;
+	private static final Object instanceLock = new Object();
+
+
+
+	public static xLog get() {
+		if(log == null) {
+			synchronized(instanceLock) {
+				if(log == null) {
+					Assert.assertFalse(Failure.hasFailed());
+					log = xLog.getRoot();
+					xLog.init();
+					xLog.getRoot().setLevel(xLevel.ALL);
+					log.publish();
+					log.title("Testing pxnCommon");
+				}
+			}
+		}
+		Assert.assertFalse(Failure.hasFailed());
+		return log;
+	}
+
+
+
+
+
+
+
+
+}
