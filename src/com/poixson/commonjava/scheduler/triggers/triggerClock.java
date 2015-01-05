3bf76eca59ba41fa08e1c5f731a2e6a56fcc0d89
lorenzop <lorenzo@poixson.com>
2015-01-05 11:11:56 -0500

    added get() and empty construct() functions to Clock and Cron scheduler classes

--- src/com/poixson/commonjava/scheduler/triggers/triggerClock.java
+++ src/com/poixson/commonjava/scheduler/triggers/triggerClock.java
@@ -5,6 +5,17 @@ public class triggerClock implements TriggerType {
 
 
 
+	public static triggerClock get(final String time) {
+		return new triggerClock(time);
+	}
+
+
+
+	public triggerClock(final String time) {
+	}
+
+
+
 	@Override
 	public long untilNextTrigger() {
 		return -1;
--- src/com/poixson/commonjava/scheduler/triggers/triggerCron.java
+++ src/com/poixson/commonjava/scheduler/triggers/triggerCron.java
@@ -5,6 +5,17 @@ public class triggerCron implements TriggerType {
 
 
 
+	public static triggerCron get(final String value) {
+		return new triggerCron(value);
+	}
+
+
+
+	public triggerCron(final String value) {
+	}
+
+
+
 	@Override
 	public long untilNextTrigger() {
 		return -1;
