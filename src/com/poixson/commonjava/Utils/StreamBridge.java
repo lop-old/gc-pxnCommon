b5c2a2775179966d7edd55472f139224be73cc81
lorenzop <lorenzo@poixson.com>
2015-01-09 21:51:55 -0500

    added StreamBridge and SystemHash utility classes from cron4j

---
+++ src/com/poixson/commonjava/Utils/StreamBridge.java
@@ -0,0 +1,160 @@
+package com.poixson.commonjava.Utils;
+/*
+ * cron4j - A pure Java cron-like scheduler
+ *
+ * Copyright (C) 2007-2010 Carlo Pelliccia (www.sauronsoftware.it)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version
+ * 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License 2.1 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License version 2.1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+/**
+ * A package-reserved utility class. It spawns a secondary thread in which the
+ * supplied {@link InputStream} instance is read, and the incoming contents are
+ * written in the supplied {@link OutputStream}.
+ * @author Carlo Pelliccia
+ */
+public class StreamBridge implements xStartable {
+
+	protected static final Set<StreamBridge> instances = new HashSet<StreamBridge>();
+	protected static final AtomicInteger count = new AtomicInteger(0);
+
+	protected final Thread thread;
+	protected volatile boolean running  = false;
+	protected volatile boolean stopping = false;
+
+	protected final InputStream  in;
+	protected final OutputStream out;
+
+
+
+	public StreamBridge(final InputStream in, final OutputStream out) {
+		synchronized(instances) {
+			instances.add(this);
+		}
+		this.in  = in;
+		this.out = out;
+		this.thread = new Thread(this);
+		this.thread.setName("StreamBridge"+Integer.toString(count.incrementAndGet()));
+	}
+	protected void remove() {
+		synchronized(instances) {
+			instances.remove(this);
+		}
+	}
+
+
+
+	@Override
+	public void run() {
+		if(this.running)  throw new RuntimeException("StreamBridge already running");
+		if(this.stopping) throw new RuntimeException("StreamBridge already stopped");
+		synchronized(this.thread) {
+			if(this.running)  throw new RuntimeException("StreamBridge already running");
+			if(this.stopping) throw new RuntimeException("StreamBridge already stopped");
+			this.running = true;
+		}
+		while(!this.stopping) {
+			final int b;
+			try {
+				b = this.in.read();
+			} catch (IOException e) {
+				e.printStackTrace();
+				break;
+			}
+			if(b == -1)
+				break;
+			try {
+				this.out.write(b);
+			} catch (IOException e) {
+				if(!this.stopping && !Thread.interrupted())
+					e.printStackTrace();
+				break;
+			}
+			utils.safeClose(this.out);
+			utils.safeClose(this.in);
+		}
+		this.running = false;
+		this.remove();
+	}
+
+
+
+	@Override
+	public void Start() {
+		this.thread.start();
+	}
+	@Override
+	public void Stop() {
+		this.stopping = true;
+		this.thread.interrupt();
+		utils.safeClose(this.out);
+		utils.safeClose(this.in);
+	}
+	@Override
+	public boolean isRunning() {
+		return this.running;
+	}
+	public boolean isAlive() {
+		return this.isRunning();
+	}
+
+
+
+	/**
+	 * Waits for this job to die.
+	 * @throws InterruptedException
+	 *             If another thread has interrupted the current thread. The
+	 *             interrupted status of the current thread is cleared when this
+	 *             exception is thrown.
+	 */
+	public void join() throws InterruptedException {
+		this.thread.join();
+	}
+	/**
+	 * Waits at most <code>millis</code> milliseconds for this thread to die. A
+	 * timeout of <code>0</code> means to wait forever.
+	 * @param millis the time to wait in milliseconds.
+	 * @throws InterruptedException
+	 *             If another thread has interrupted the current thread. The
+	 *             interrupted status of the current thread is cleared when this
+	 *             exception is thrown.
+	 */
+	public void join(final long millis) throws InterruptedException {
+		this.thread.join(millis);
+	}
+	/**
+	 * @param millis the time to wait in milliseconds.
+	 * @param nanos 0-999999 additional nanoseconds to wait.
+	 * @throws IllegalArgumentException if the value of millis is negative the
+	 *             value of nanos is not in the range 0-999999.
+	 * @throws InterruptedException
+	 *             If another thread has interrupted the current thread. The
+	 *             interrupted status of the current thread is cleared when this
+	 *             exception is thrown.
+	 */
+	public void join(final long millis, final int nanos) throws IllegalArgumentException, InterruptedException {
+		this.thread.join(millis, nanos);
+	}
+
+
+
+}
---
+++ src/com/poixson/commonjava/Utils/SystemHash.java
@@ -0,0 +1,197 @@
+package com.poixson.commonjava.Utils;
+/*
+ * cron4j - A pure Java cron-like scheduler
+ *
+ * Copyright (C) 2007-2010 Carlo Pelliccia (www.sauronsoftware.it)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version
+ * 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License 2.1 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License version 2.1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+
+/**
+ * A GUID generator.
+ * @author Carlo Pelliccia
+ */
+public final class SystemHash {
+	private SystemHash() {}
+
+	private static final String DESCRIPTOR = getMachineDescriptor();
+
+
+
+	/**
+	 * Generates a GUID (48 chars).
+	 * @return The generated GUID.
+	 */
+	public static String generate() {
+		final int rnd = utilsNumbers.getRandom(0, Integer.MAX_VALUE);
+		final StringBuffer str = new StringBuffer();
+		encode(str, DESCRIPTOR);
+		encode(str, Runtime.getRuntime());
+		encode(str, Thread.currentThread());
+		encode(str, System.currentTimeMillis());
+		encode(str, rnd);
+		return str.toString();
+	}
+
+
+
+	// ==================================================
+
+
+
+	/**
+	 * Calculates a machine id, as an integer value.
+	 * @return The calculated machine id.
+	 */
+	private static String getMachineDescriptor() {
+		final StringBuffer desc = new StringBuffer();
+		desc.append(System.getProperty("os.name"));
+		desc.append("::");
+		desc.append(System.getProperty("os.arch"));
+		desc.append("::");
+		desc.append(System.getProperty("os.version"));
+		desc.append("::");
+		desc.append(System.getProperty("user.name"));
+		desc.append("::");
+		final StringBuffer b = buildNetworkInterfaceDescriptor();
+		if(b != null) {
+			desc.append(b);
+		} else {
+			InetAddress addr = null;
+			try {
+				addr = InetAddress.getLocalHost();
+				desc.append(addr.getHostAddress());
+			} catch (UnknownHostException ignore) {}
+		}
+		return desc.toString();
+	}
+	/**
+	 * Builds a descriptor fragment using the {@link NetworkInterface} class,
+	 * available since Java 1.4.
+	 * @return A descriptor fragment, or null if the method fails.
+	 */
+	private static StringBuffer buildNetworkInterfaceDescriptor() {
+		final Enumeration<NetworkInterface> e;
+		try {
+			e = NetworkInterface.getNetworkInterfaces();
+		} catch (Throwable ignore) {
+			return null;
+		}
+		final StringBuffer str = new StringBuffer();
+		while(e.hasMoreElements()) {
+			NetworkInterface net = (NetworkInterface) e.nextElement();
+			final StringBuffer str1 = getMACAddressDescriptor(net);
+			final StringBuffer str2 = getInetAddressDescriptor(net);
+			final StringBuffer str3 = new StringBuffer();
+			if(str1 != null)
+				str3.append(str1);
+			if(str2 != null) {
+				if(str3.length() > 0)
+					str3.append('=');
+				str3.append(str2);
+			}
+			if(str3.length() > 0) {
+				if(str.length() > 0)
+					str.append(';');
+				str.append(str3);
+			}
+		}
+		return str;
+	}
+	/**
+	 * Builds a descriptor fragment using the machine MAC address.
+	 * @return A descriptor fragment, or null if the method fails.
+	 */
+	private static StringBuffer getMACAddressDescriptor(final NetworkInterface net) {
+		byte[] haddr = null;
+		try {
+			haddr = net.getHardwareAddress();
+		} catch (Throwable ignore) {}
+		final StringBuffer str = new StringBuffer();
+		if(haddr != null) {
+			for(int i = 0; i < haddr.length; i++) {
+				if(str.length() > 0)
+					str.append("-");
+				final String hex = Integer.toHexString(0xff & haddr[i]);
+				if(hex.length() == 1)
+					str.append('0');
+				str.append(hex);
+			}
+		}
+		return str;
+	}
+	/**
+	 * Builds a descriptor fragment using the machine inet address.
+	 * @return A descriptor fragment, or null if the method fails.
+	 */
+	private static StringBuffer getInetAddressDescriptor(NetworkInterface net) {
+		final StringBuffer str = new StringBuffer();
+		final Enumeration<InetAddress> e = net.getInetAddresses();
+		while(e.hasMoreElements()) {
+			final InetAddress addr = e.nextElement();
+			if(str.length() > 0)
+				str.append(',');
+			str.append(addr.getHostAddress());
+		}
+		return str;
+	}
+
+
+
+	// ==================================================
+
+
+
+	/**
+	 * Encodes an object and appends it to the buffer.
+	 * @param str The buffer.
+	 * @param obj The object.
+	 */
+	private static void encode(final StringBuffer str, final Object obj) {
+		encode(str, obj.hashCode());
+	}
+	/**
+	 * Encodes an integer value and appends it to the buffer.
+	 * @param str The buffer.
+	 * @param value The value.
+	 */
+	private static void encode(final StringBuffer str, final int value) {
+		final String hex = Integer.toHexString(value);
+		final int hexSize = hex.length();
+		for(int i = 8; i > hexSize; i--)
+			str.append('0');
+		str.append(hex);
+	}
+	/**
+	 * Encodes a long value and appends it to the buffer.
+	 * @param str The buffer.
+	 * @param value The value.
+	 */
+	private static void encode(final StringBuffer str, final long value) {
+		final String hex = Long.toHexString(value);
+		final int hexSize = hex.length();
+		for(int i = 16; i > hexSize; i--)
+			str.append('0');
+		str.append(hex);
+	}
+
+
+
+}
