bfb6e2d38fab7aac936f04d4dfb706e54aa5324f
lorenzop <lorenzo@poixson.com>
2015-01-10 06:39:26 -0500

    finished xThreadPool junit testing classes

---
+++ test/com/poixson/commonjava/utils/xThreadPool/xThreadPoolFiringTest.java
@@ -0,0 +1,97 @@
+package com.poixson.commonjava.utils.xThreadPool;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.poixson.commonjava.Failure;
+import com.poixson.commonjava.Utils.utilsThread;
+import com.poixson.commonjava.Utils.xThreadPool;
+import com.poixson.commonjava.logger.xLogTest;
+import com.poixson.commonjava.xLogger.xLog;
+
+
+public class xThreadPoolFiringTest {
+
+	private static final int THREAD_COUNT_SHORT = 1;
+	private static final int THREAD_COUNT_LONG  = 20;
+	private static final int TASK_COUNT_MAIN  = 1000;
+	private static final int TASK_COUNT_SHORT = 1000;
+	private static final int TASK_COUNT_LONG  = 1000;
+
+	private static volatile Thread mainThread = null;
+
+	private final xThreadPool pool_main;
+	private final xThreadPool pool_short;
+	private final xThreadPool pool_long;
+
+	private volatile xThreadQueuer queuer_main  = null;
+	private volatile xThreadQueuer queuer_short = null;
+	private volatile xThreadQueuer queuer_long  = null;
+
+
+
+	public xThreadPoolFiringTest() {
+		assertHasntFailed();
+		log().info("Starting up thread pools..");
+		this.pool_main = xThreadPool.get();
+		this.pool_short = xThreadPool.get("short", THREAD_COUNT_SHORT);
+		this.pool_long  = xThreadPool.get("long",  THREAD_COUNT_LONG);
+		assertHasntFailed();
+		// start main thread pool
+		mainThread = new Thread() {
+			@Override
+			public void run() {
+				xThreadPool.get().run();
+			}
+		};
+		mainThread.start();
+		assertHasntFailed();
+		// start more thread pools
+		this.pool_short.Start();
+		this.pool_long.Start();
+		utilsThread.Sleep(20);
+		assertHasntFailed();
+	}
+
+
+
+	@Test
+	public void StartFiringTest() {
+		assertHasntFailed();
+		// task producers
+		this.queuer_main  = new xThreadQueuer(this.pool_main,  TASK_COUNT_MAIN);
+		this.queuer_short = new xThreadQueuer(this.pool_short, TASK_COUNT_SHORT);
+		this.queuer_long  = new xThreadQueuer(this.pool_long,  TASK_COUNT_LONG);
+		assertHasntFailed();
+		// start producers
+		log().info("Starting task producers..");
+		log().publish();
+		xThreadQueuer.runAll();
+		log().publish();
+		// ensure all finished
+		Assert.assertTrue(this.queuer_main.hasFinished());
+		Assert.assertTrue(this.queuer_short.hasFinished());
+		Assert.assertTrue(this.queuer_long.hasFinished());
+		assertHasntFailed();
+		log().info("xThreadPool Tests Passed!");
+	}
+
+
+
+	public static void assertHasntFailed() {
+		Assert.assertFalse(
+			"App Failed!",
+			Failure.hasFailed()
+		);
+	}
+
+
+
+	// logger
+	public static xLog log() {
+		return xLogTest.get();
+	}
+
+
+
+}
---
+++ test/com/poixson/commonjava/utils/xThreadPool/xThreadQueuer.java
@@ -0,0 +1,127 @@
+package com.poixson.commonjava.utils.xThreadPool;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import org.junit.Assert;
+
+import com.poixson.commonjava.Utils.CoolDown;
+import com.poixson.commonjava.Utils.utilsThread;
+import com.poixson.commonjava.Utils.xThreadPool;
+import com.poixson.commonjava.logger.xLogTest;
+import com.poixson.commonjava.xLogger.xLog;
+
+
+class xThreadQueuer {
+
+	private static final Set<xThreadQueuer> instances = new CopyOnWriteArraySet<xThreadQueuer>();
+	private static volatile boolean allFinished = false;
+	private static final CoolDown maxRunTime = CoolDown.get("10s");
+
+	private final xThreadPool pool;
+	private final int maxTaskCount;
+	private volatile int currentTaskCount = 0;
+	private volatile boolean finished = false;
+
+
+
+	public xThreadQueuer(final xThreadPool pool, final int maxTaskCount) {
+		this.pool = pool;
+		this.maxTaskCount = maxTaskCount;
+		this.currentTaskCount = 0;
+		this.logLocal().info("Testing with "+
+				(pool.getMaxThreads() == 0
+					? "MAIN thread"
+					: Integer.toString(pool.getMaxThreads())+" threads"
+				)+" and "+
+				Integer.toString(maxTaskCount)+" tasks");
+		// reset cooldown
+		maxRunTime.reset();
+		maxRunTime.runAgain();
+		this.finished = false;
+		instances.add(this);
+	}
+
+
+
+	public boolean hasFinished() {
+		return this.finished;
+	}
+	public static boolean allFinished() {
+		return allFinished;
+	}
+
+
+
+	public static void runAll() {
+		// reset cooldown
+		maxRunTime.reset();
+		maxRunTime.runAgain();
+		boolean allfinished;
+		boolean hasadded;
+		while(true) {
+			allfinished = true;
+			hasadded = false;
+			// run the instances
+			final Iterator<xThreadQueuer> it = instances.iterator();
+			while(it.hasNext()) {
+				final xThreadQueuer queuer = it.next();
+				if(!queuer.hasFinished()) {
+					// run instance
+					if(queuer.queueMore())
+						hasadded = true;
+					allfinished = false;
+				}
+			}
+			if(allfinished)
+				break;
+			// max run timeout
+			Assert.assertFalse(
+				"Run Timeout!",
+				maxRunTime.runAgain()
+			);
+			if(!hasadded)
+				utilsThread.Sleep(10L);
+		}
+		xLogTest.get().info("Finished xThreadPool Tests");
+		allFinished = true;
+	}
+
+
+
+	public boolean queueMore() {
+		if(this.maxTaskCount <= this.currentTaskCount) {
+			while(!this.pool.isEmpty())
+				utilsThread.Sleep(10L);
+			this.logLocal().info("Finished testing pool");
+			this.finished = true;
+			return false;
+		}
+		if(Math.min(1, this.pool.getMaxThreads()) + 5 <= this.pool.getActiveCount())
+			return false;
+		// queue another task
+		this.pool.runLater(
+			new Runnable() {
+				@Override
+				public void run() {
+					if(xThreadPool.DETAILED_LOGGING)
+						logLocal().warning("TICK");
+				}
+			}
+		);
+		this.currentTaskCount++;
+		return true;
+	}
+
+
+
+	// logger
+	public xLog logLocal() {
+		return xLogTest.get()
+				.get(this.pool.getName());
+	}
+
+
+
+}
