d406f428ab57b938a0fb80b2955f2502f19f1fff
lorenzop <lorenzo@poixson.com>
2015-08-20 03:18:09 -0400

    added test class for utilsProc

---
+++ tests/com/poixson/commonjava/utils/utilsProcTest.java
@@ -0,0 +1,43 @@
+package com.poixson.commonjava.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.poixson.commonjava.xLogTest;
+import com.poixson.commonjava.Utils.utilsProc;
+
+
+public class utilsProcTest {
+	static final String TEST_GET_PID = "getPid()";
+
+
+
+	@Test
+	public void testGetPid() {
+		xLogTest.testStart(TEST_GET_PID);
+		final int pid = utilsProc.getPid();
+		Assert.assertTrue("Failed to get pid! "+Integer.toString(pid), pid > 0);
+		xLogTest.publish("Found PID: "+Integer.toString(pid));
+		xLogTest.testPassed(TEST_GET_PID);
+	}
+
+
+
+//	@Test
+//	public void testLockInstance() {
+//		final String LOCK_FILE = "test.lock";
+//		// get lock
+//		{
+//			final boolean result = utilsProc.lockInstance(LOCK_FILE);
+//			Assert.assertTrue("Failed to lock file: "+LOCK_FILE, result);
+//		}
+//		// expect fail
+//		{
+//			final boolean result = utilsProc.lockInstance(LOCK_FILE);
+//			Assert.assertFalse("Expected fail returned true: "+LOCK_FILE, result);
+//		}
+//	}
+
+
+
+}
