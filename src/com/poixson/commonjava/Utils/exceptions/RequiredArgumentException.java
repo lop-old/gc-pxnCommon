1222492017c5f785be83f0983b4a0d63e96c7e95
lorenzop <lorenzo@poixson.com>
2015-11-02 11:59:02 -0500

    added RequireArgumentException

---
+++ src/com/poixson/commonjava/Utils/exceptions/RequiredArgumentException.java
@@ -0,0 +1,19 @@
+package com.poixson.commonjava.Utils.exceptions;
+
+
+public class RequiredArgumentException extends NullPointerException {
+	private static final long serialVersionUID = 1L;
+
+
+
+	public RequiredArgumentException(final String argName) {
+		super( (new StringBuilder())
+				.append(argName)
+				.append(" argument is required!")
+				.toString()
+		);
+	}
+
+
+
+}
