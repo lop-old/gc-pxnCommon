7ad772397826d4f85f062bcd1d37eb5c85fbafec
lorenzop <lorenzo@poixson.com>
2015-11-18 18:11:04 -0500

    added RunnableMultiplier and xRunnableMultiplier classes

---
+++ src/com/poixson/commonjava/remapped/RunnableMultiplier.java
@@ -0,0 +1,38 @@
+package com.poixson.commonjava.remapped;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+
+public class RunnableMultiplier implements Runnable {
+
+	private final Set<Runnable> runs = new CopyOnWriteArraySet<Runnable>();
+
+
+
+	public RunnableMultiplier() {
+		super();
+	}
+
+
+
+	public void add(final Runnable run) {
+		if(run == null) throw new NullPointerException("run argument is required!");
+		this.runs.add(run);
+	}
+
+
+
+	@Override
+	public void run() {
+		final Iterator<Runnable> it = this.runs.iterator();
+		while(it.hasNext()) {
+			final Runnable run = it.next();
+			run.run();
+		}
+	}
+
+
+
+}
---
+++ src/com/poixson/commonjava/remapped/xRunnableMultiplier.java
@@ -0,0 +1,52 @@
+package com.poixson.commonjava.remapped;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import com.poixson.commonjava.Utils.xRunnable;
+
+
+public class xRunnableMultiplier extends xRunnable {
+
+	private final Set<xRunnable> runs = new CopyOnWriteArraySet<xRunnable>();
+
+
+
+	public xRunnableMultiplier() {
+		super();
+	}
+	public xRunnableMultiplier(final String taskName) {
+		super(taskName);
+	}
+	public xRunnableMultiplier(final xRunnable run) {
+		super(run);
+	}
+	public xRunnableMultiplier(final Runnable run) {
+		super(run);
+	}
+	public xRunnableMultiplier(final String taskName, final Runnable run) {
+		super(taskName, run);
+	}
+
+
+
+	public void add(final xRunnable run) {
+		if(run == null) throw new NullPointerException("run argument is required!");
+		this.runs.add(run);
+	}
+
+
+
+	@Override
+	public void run() {
+		final Iterator<xRunnable> it = this.runs.iterator();
+		while(it.hasNext()) {
+			final xRunnable run = it.next();
+			run.run();
+		}
+	}
+
+
+
+}
