65bed5c9df9d1e5c1d37f0420352de8524030f29
lorenzop <lorenzo@poixson.com>
2015-11-19 23:35:03 -0500

    added LongRef and DoubleRef classes

---
+++ src/com/poixson/commonjava/Utils/byRef/DoubleRef.java
@@ -0,0 +1,41 @@
+package com.poixson.commonjava.Utils.byRef;
+
+import com.poixson.commonjava.Utils.exceptions.RequiredArgumentException;
+
+
+public class DoubleRef {
+
+	public volatile double value = 0;
+
+
+
+	public DoubleRef(final double value) {
+		this.value = value;
+	}
+	public DoubleRef() {
+	}
+
+
+
+	public void value(final double value) {
+		this.value = value;
+	}
+	public void value(final Double value) {
+		if(value == null) throw new RequiredArgumentException("value");
+		this.value = value.doubleValue();
+	}
+	public double value() {
+		return this.value;
+	}
+
+
+	public void increment() {
+		this.value++;
+	}
+	public void decrement() {
+		this.value--;
+	}
+
+
+
+}
---
+++ src/com/poixson/commonjava/Utils/byRef/LongRef.java
@@ -0,0 +1,41 @@
+package com.poixson.commonjava.Utils.byRef;
+
+import com.poixson.commonjava.Utils.exceptions.RequiredArgumentException;
+
+
+public class LongRef {
+
+	public volatile long value = 0;
+
+
+
+	public LongRef(final long value) {
+		this.value = value;
+	}
+	public LongRef() {
+	}
+
+
+
+	public void value(final long value) {
+		this.value = value;
+	}
+	public void value(final Long value) {
+		if(value == null) throw new RequiredArgumentException("value");
+		this.value = value.longValue();
+	}
+	public long value() {
+		return this.value;
+	}
+
+
+	public void increment() {
+		this.value++;
+	}
+	public void decrement() {
+		this.value--;
+	}
+
+
+
+}
