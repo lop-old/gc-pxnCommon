d0657af225ee4d8a8fe72cedb8eb5c0b12fccc9a
lorenzop <lorenzo@poixson.com>
2017-04-26 14:44:02 -0400

    cleaned up and finished HangCatcher class

--- src/com/poixson/utils/HangCatcher.java
+++ src/com/poixson/utils/HangCatcher.java
@@ -1,47 +1,66 @@
-/*
-package com.poixson.commonjava.Utils.threads;
+package com.poixson.utils;
 
 import java.io.PrintStream;
 
 import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
 
-import com.poixson.commonjava.Utils.CoolDown;
 import com.poixson.commonjava.Utils.Keeper;
-import com.poixson.commonjava.Utils.utilsThread;
-import com.poixson.commonjava.Utils.xStartable;
-import com.poixson.commonjava.Utils.xTime;
 
 
 public class HangCatcher implements xStartable {
 
-	static final xTime TIMEOUT = xTime.get("10s");
-	static final xTime SLEEP   = xTime.get("100n");
+	private static final String DEFAULT_TIMEOUT = "10s";
+	private static final String DEFAULT_SLEEP   = "100n";
 
 	protected volatile Thread thread = null;
 	protected volatile boolean cancel = false;
 
 	protected final CoolDown cool;
+	protected final xTime sleep;
 
 
 
-	public static HangCatcher get() {
-		final HangCatcher catcher = new HangCatcher();
-		Keeper.add(catcher);
-		return catcher;
-	}
 	public HangCatcher() {
-		this.cool = CoolDown.get(TIMEOUT);
+		this(
+			DEFAULT_TIMEOUT,
+			DEFAULT_SLEEP
+		);
+	}
+	public HangCatcher(final String timeoutStr, final String sleepStr) {
+		this(
+			xTime.get(
+				Utils.isEmpty(timeoutStr)
+				? DEFAULT_TIMEOUT
+				: timeoutStr
+			),
+			xTime.get(
+				Utils.isEmpty(sleepStr)
+				? DEFAULT_SLEEP
+				: sleepStr
+			)
+		);
+	}
+	public HangCatcher(final xTime timeout, final xTime sleep) {
+		this.cool = CoolDown.get(
+			timeout == null
+			? xTime.get(DEFAULT_TIMEOUT)
+			: timeout
+		);
+		this.sleep = (
+			sleep == null
+			? xTime.get(DEFAULT_SLEEP)
+			: sleep
+		);
 	}
 
 
 
 	@Override
 	public void Start() {
-		if(this.cancel)
-			return;
+		if (this.cancel) return;
 		this.resetTimeout();
-		if(this.thread == null) {
+		if (this.thread == null) {
 			this.thread = new Thread(this);
 			this.thread.setName("HangCatcher");
 			this.thread.setDaemon(true);
@@ -66,10 +85,10 @@ public class HangCatcher implements xStartable {
 				out.println(Ansi.ansi().a(" ").fg(Ansi.Color.RED).a("*  Shutdown has hung!  *").reset().a(" "));
 				out.println(Ansi.ansi().a(" ").fg(Ansi.Color.RED).a("************************").reset().a(" "));
 				out.println();
-				utilsThread.displayStillRunning();
+				ThreadUtils.displayStillRunning();
 				System.exit(1);
 			}
-			utilsThread.Sleep(SLEEP);
+			ThreadUtils.Sleep(this.sleep);
 		}
 		this.cancel = true;
 		this.thread = null;
@@ -86,4 +105,3 @@ public class HangCatcher implements xStartable {
 
 
 }
-*/
