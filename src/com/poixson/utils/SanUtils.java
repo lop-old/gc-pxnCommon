42bd3515dc437274aef606246c92c9f04fd5c49f
lorenzop <lorenzo@poixson.com>
2017-05-05 05:30:30 -0400

    added safeAlphaNum() safeAlphaNumUnderscore() and safeFileName() functions to SanUtils class

--- src/com/poixson/utils/SanUtils.java
+++ src/com/poixson/utils/SanUtils.java
@@ -17,10 +17,25 @@ public final class SanUtils {
 		if (text.isEmpty()) return "";
 		return text.replaceAll("[^a-zA-Z0-9]+", "");
 	}
+	public static boolean safeAlphaNum(final String text) {
+		if (text == null)
+			return true;
+		final String safeText = AlphaNum(text);
+		return text.equals(safeText);
+	}
+
+
+
 	public static String AlphaNumUnderscore(final String text) {
 		if (text == null)   return null;
 		if (text.isEmpty()) return "";
 		return text.replaceAll("[^a-zA-Z0-9\\-\\_\\.]+", "");
+	}
+	public static boolean safeAlphaNumUnderscore(final String text) {
+		if (text == null)
+			return true;
+		final String safeText = AlphaNumUnderscore(text);
+		return text.equals(safeText);
 	}
 
 
@@ -35,7 +50,14 @@ public final class SanUtils {
 
 
 	public static String FileName(final String text) {
+		if (Utils.isBlank(text)) return null;
 		return text.replaceAll("[^a-zA-Z0-9\\._]+", "_");
+	}
+	public static boolean safeFileName(final String text) {
+		if (text == null)
+			return true;
+		final String safeText = FileName(text);
+		return text.equals(safeText);
 	}
 
 
