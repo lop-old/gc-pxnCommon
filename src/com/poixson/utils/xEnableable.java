461cb8696a715d0700a981aa4c21aa72c8075a05
lorenzop <lorenzo@poixson.com>
2017-05-07 01:50:29 -0400

    added xEnableable interface

---
+++ src/com/poixson/utils/xEnableable.java
@@ -0,0 +1,13 @@
+package com.poixson.utils;
+
+
+public interface xEnableable {
+
+
+	public boolean isEnabled();
+	public boolean notEnabled();
+	public void setEnabled();
+	public void setEnabled(final boolean enabled);
+
+
+}
