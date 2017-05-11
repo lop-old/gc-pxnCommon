69cbdd80e2beb11747fdd734c40823f64a9d836c
lorenzop <lorenzo@poixson.com>
2017-05-11 13:36:49 -0400

    added setDisabled() abstract function to xEnableable interface

--- src/com/poixson/utils/xEnableable.java
+++ src/com/poixson/utils/xEnableable.java
@@ -7,6 +7,7 @@ public interface xEnableable {
 	public boolean isEnabled();
 	public boolean notEnabled();
 	public void setEnabled();
+	public void setDisabled();
 	public void setEnabled(final boolean enabled);
 
 
