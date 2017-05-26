387f583f337ada0626b40e42464e5d81e8f5a50d
lorenzop <lorenzo@poixson.com>
2017-05-26 08:41:30 -0400

    added (Object...args) argument to xLogPrinting functions for easy variable insertion using {} in msg

--- src/com/poixson/utils/xLogger/xLogPrinting.java
+++ src/com/poixson/utils/xLogger/xLogPrinting.java
@@ -28,9 +28,35 @@ public abstract class xLogPrinting {
 
 
 
+	public static String ReplaceArgs(final String msg, final Object[] args) {
+		if (msg == null)
+			return null;
+		if (args.length == 0)
+			return msg;
+		if (args.length == 1) {
+			return msg.replaceFirst("\\{\\}", args[0].toString());
+		}
+		String str = msg;
+		for (final Object arg : args) {
+			str = str.replaceFirst("\\{\\}", arg.toString());
+		}
+		return str;
+	}
+
+
+
 	// publish message
 	public abstract void publish(final xLogRecord record);
 	public abstract void publish(final String msg);
+	public void publish(final String msg, final Object... args) {
+		this.publish(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
+	// publish message with level
 	public void publish(final xLevel level, final String msg) {
 		if (level == null) {
 			this.publish(msg);
@@ -44,6 +70,15 @@ public abstract class xLogPrinting {
 			)
 		);
 	}
+	public void publish(final xLevel level, final String msg, final Object... args) {
+		this.publish(
+			level,
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	public void publish() {
 		publish("");
 	}
@@ -82,6 +117,11 @@ public abstract class xLogPrinting {
 		if (list == null) return;
 		this.title(list.toArray(new String[0]));
 	}
+	public void title(final String msg, final Object... args) {
+		this.title(
+			ReplaceArgs(msg, args)
+		);
+	}
 
 
 
@@ -102,6 +142,12 @@ public abstract class xLogPrinting {
 			str.toString()
 		);
 	}
+	public void trace(final Throwable e, final String msg, final Object... args) {
+		this.trace(
+			e,
+			ReplaceArgs(msg, args)
+		);
+	}
 
 
 
@@ -109,9 +155,22 @@ public abstract class xLogPrinting {
 	public void stdout(final String msg) {
 		this.publish(xLevel.STDOUT, msg);
 	}
+	public void stdout(final String msg, final Object... args) {
+		this.stdout(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	// stderr
 	public void stderr(final String msg) {
 		this.publish(xLevel.STDERR, msg);
+	}
+	public void stderr(final String msg, final Object... args) {
+		this.stderr(
+			ReplaceArgs(msg, args)
+		);
 	}
 
 
@@ -120,37 +179,106 @@ public abstract class xLogPrinting {
 	public void detail(final String msg) {
 		this.publish(xLevel.DETAIL, msg);
 	}
+	public void detail(final String msg, final Object... args) {
+		this.detail(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	// finest
 	public void finest(final String msg) {
 		this.publish(xLevel.FINEST, msg);
 	}
+	public void finest(final String msg, final Object... args) {
+		this.finest(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	// finer
 	public void finer(final String msg) {
 		this.publish(xLevel.FINER, msg);
 	}
+	public void finer(final String msg, final Object... args) {
+		this.finer(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	// fine
 	public void fine(final String msg) {
 		this.publish(xLevel.FINE, msg);
 	}
+	public void fine(final String msg, final Object... args) {
+		this.fine(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	// stats
 	public void stats(final String msg) {
 		this.publish(xLevel.STATS, msg);
 	}
+	public void stats(final String msg, final Object... args) {
+		this.stats(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	// info
 	public void info(final String msg) {
 		this.publish(xLevel.INFO, msg);
 	}
+	public void info(final String msg, final Object... args) {
+		this.info(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	// warning
 	public void warning(final String msg) {
 		this.publish(xLevel.WARNING, msg);
 	}
+	public void warning(final String msg, final Object... args) {
+		this.warning(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	// severe
 	public void severe(final String msg) {
 		this.publish(xLevel.SEVERE, msg);
 	}
+	public void severe(final String msg, final Object... args) {
+		this.severe(
+			ReplaceArgs(msg, args)
+		);
+	}
+
+
+
 	// fatal
 	public void fatal(final String msg) {
 		this.publish(xLevel.FATAL, msg);
+	}
+	public void fatal(final String msg, final Object... args) {
+		this.fatal(
+			ReplaceArgs(msg, args)
+		);
 	}
 
 
