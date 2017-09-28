01800758ea4978b5ccfb9f790e7633e01075b480
lorenzop <lorenzo@poixson.com>
2017-09-28 09:28:56 -0400

    added IORuntimeException

---
+++ src/com/poixson/utils/exceptions/IORuntimeException.java
@@ -0,0 +1,24 @@
+package com.poixson.utils.exceptions;
+
+
+public class IORuntimeException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+
+
+	public IORuntimeException() {
+		super();
+	}
+	public IORuntimeException(final String msg) {
+		super(msg);
+	}
+	public IORuntimeException(final String msg, final Throwable e) {
+		super(msg, e);
+	}
+	public IORuntimeException(final Throwable e) {
+		super(e);
+	}
+
+
+
+}
