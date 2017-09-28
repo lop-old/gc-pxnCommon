13d65487928da5bd251a55304aa5cf89cba7883c
lorenzop <lorenzo@poixson.com>
2017-09-28 15:17:36 -0400

    rewrote NativeUtils class and added new NativeLoader helper class

---
+++ src/com/poixson/utils/NativeLoader.java
@@ -0,0 +1,302 @@
+package com.poixson.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.SoftReference;
+
+import com.poixson.utils.exceptions.IORuntimeException;
+import com.poixson.utils.exceptions.RequiredArgumentException;
+import com.poixson.utils.xLogger.xLog;
+
+
+public class NativeLoader {
+
+	public static boolean DEFAULT_ENABLE_EXTRACT = false;
+	public static boolean DEFAULT_ENABLE_REPLACE = false;
+
+	protected ErrorMode errorMode = null;
+	protected Class<?> classRef   = null;
+
+	protected String fileName = null;
+	protected String libPath  = null;
+	protected String resPath  = null;
+
+	protected Boolean enableExtract = null;
+	protected Boolean enableReplace = null;
+
+
+
+	public static NativeLoader get() {
+		return new NativeLoader();
+	}
+	public NativeLoader() {
+	}
+
+
+
+	// extract/load library
+	public boolean LoadLibrary(final String fileName) {
+		if (Utils.notEmpty(fileName)) {
+			this.setFileName(fileName);
+		}
+		return this.LoadLibrary();
+	}
+	public boolean LoadLibrary() {
+		final ErrorMode errorMode = this.getErrorMode();
+		final String fileName = this.getFileName();
+		if (Utils.isEmpty(fileName)) {
+			if (errorMode.equals(ErrorMode.EXCEPTION)) {
+				throw new RequiredArgumentException("fileName");
+			} else
+			if (errorMode.equals(ErrorMode.LOG)) {
+				this.log().severe("Library fileName is missing!");
+			}
+			return false;
+		}
+		final Class<?> classRef   = this.getClassRef();
+		final String libPath = this.getLibrariesPath();
+		final String resPath = this.getResourcesPath();
+		final boolean enableExtract = this.getEnableExtract();
+		final boolean enableReplace = this.getEnableReplace();
+		final String filePath =
+			FileUtils.MergePaths(
+				".",
+				libPath,
+				fileName
+			);
+		final File outFile = new File(filePath);
+		// create library directory
+		{
+			final File dir = new File(libPath);
+			if (!dir.isDirectory()) {
+				if (dir.mkdirs()) {
+					this.log().info("Created libraries dir: {}", libPath);
+				} else {
+					if (errorMode.equals(ErrorMode.EXCEPTION)) {
+						throw new IORuntimeException("Failed to create directory: "+libPath);
+					} else
+					if (errorMode.equals(ErrorMode.LOG)) {
+						this.log().severe("Failed to create directory: {}", libPath);
+					}
+					return false;
+				}
+			}
+		}
+		// remove existing file
+		if (enableExtract && enableReplace) {
+			final boolean exists = outFile.isFile();
+			if (exists) {
+				if (enableReplace && enableExtract) {
+					this.log().fine("Replacing existing library file: {}", fileName);
+					outFile.delete();
+				}
+			}
+		}
+		// extract file
+		if (enableExtract) {
+			final boolean exists = outFile.isFile();
+			if (!exists) {
+				try {
+					NativeUtils.ExtractLibrary(
+						libPath,
+						resPath,
+						fileName,
+						classRef
+					);
+				} catch (IOException e) {
+					this.log().severe(e.getMessage());
+					if (errorMode.equals(ErrorMode.EXCEPTION)) {
+						throw new IORuntimeException("Failed to extract library file: "+filePath, e);
+					} else
+					if (errorMode.equals(ErrorMode.LOG)) {
+						this.log().severe("Failed to extract library file: {}", filePath);
+					}
+					return false;
+				}
+			}
+		}
+		// load library
+		{
+			final boolean exists = outFile.isFile();
+			if (!exists) {
+				if (errorMode.equals(ErrorMode.EXCEPTION)) {
+					throw new IORuntimeException("Library file not found: "+filePath);
+				} else
+				if (errorMode.equals(ErrorMode.LOG)) {
+					this.log().severe("Library file not found: {}", filePath);
+				}
+				return false;
+			}
+			try {
+				NativeUtils.LoadLibrary(filePath);
+			} catch (SecurityException e) {
+				this.log().severe(e.getMessage());
+				if (errorMode.equals(ErrorMode.EXCEPTION)) {
+					throw e;
+				} else
+				if (errorMode.equals(ErrorMode.LOG)) {
+					this.log().severe("Failed to load library: {}  {}", filePath, e.getMessage());
+				}
+				return false;
+			} catch (UnsatisfiedLinkError e) {
+				this.log().severe(e.getMessage());
+				if (errorMode.equals(ErrorMode.EXCEPTION)) {
+					throw e;
+				} else
+				if (errorMode.equals(ErrorMode.LOG)) {
+					this.log().severe("Failed to load library: {}  {}", filePath, e.getMessage());
+				}
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+
+	// error mode
+	public ErrorMode getErrorMode() {
+		final ErrorMode mode = this.errorMode;
+		return (
+			mode == null
+			? ErrorMode.LOG
+			: mode
+		);
+	}
+	public NativeLoader setErrorMode(final ErrorMode mode) {
+		this.errorMode = mode;
+		return this;
+	}
+
+
+
+	// library file name
+	public String getFileName() {
+		String fileName = this.fileName;
+		if (Utils.isEmpty(fileName))
+			return null;
+//TODO: append file extension per os
+//		switch (os) {
+//		case LINUX:
+//			fileName = StringUtils.ForceEnds(".so", fileName);
+//			break;
+//		}
+		return fileName;
+	}
+	public NativeLoader setFileName(final String fileName) {
+		this.fileName = fileName;
+		return this;
+	}
+
+
+
+	// class reference
+	public Class<?> getClassRef() {
+		final Class<?> clss = this.classRef;
+		return (
+			clss == null
+			? NativeLoader.class
+			: clss
+		);
+	}
+	public NativeLoader setClassRef(final Class<?> clss) {
+		this.classRef = clss;
+		return this;
+	}
+
+
+
+	// resource library path
+	public String getResourcesPath() {
+		final String path = this.resPath;
+		return (
+			Utils.isEmpty(path)
+			? null
+			: StringUtils.ForceStarts("/", path)
+		);
+	}
+	public NativeLoader setResourcesPath(final String path) {
+		this.resPath = path;
+		return this;
+	}
+
+
+
+	// extracted library path
+	public String getLibrariesPath() {
+		final String path = this.libPath;
+		return (
+			Utils.isEmpty(path)
+			? "."
+			: path
+		);
+	}
+	public NativeLoader setLibrariesPath(final String path) {
+		this.libPath = path;
+		return this;
+	}
+
+
+
+	// enable extract file
+	public boolean getEnableExtract() {
+		final Boolean enabled = this.enableExtract;
+		return (
+			enabled == null
+			? DEFAULT_ENABLE_EXTRACT
+			: enabled.booleanValue()
+		);
+	}
+	public NativeLoader enableExtract() {
+		return this.enableExtract(true);
+	}
+	public NativeLoader disableExtract() {
+		return this.enableExtract(false);
+	}
+	public NativeLoader enableExtract(final boolean enable) {
+		this.enableExtract = Boolean.valueOf(enable);
+		return this;
+	}
+
+
+
+	// enable replace file
+	public boolean getEnableReplace() {
+		final Boolean enabled = this.enableReplace;
+		return (
+			enabled == null
+			? DEFAULT_ENABLE_REPLACE
+			: enabled.booleanValue()
+		);
+	}
+	public NativeLoader enableReplace() {
+		return this.enableReplace(true);
+	}
+	public NativeLoader disableReplace() {
+		return this.enableReplace(false);
+	}
+	public NativeLoader enableReplace(final boolean enable) {
+		this.enableReplace = Boolean.valueOf(enable);
+		return this;
+	}
+
+
+
+	// logger
+	private volatile SoftReference<xLog> _log = null;
+	public xLog log() {
+		if (this._log != null) {
+			final xLog log = this._log.get();
+			if (log != null)
+				return log;
+		}
+		final xLog log =
+			xLog.getRoot()
+				.get("LibLoader");
+		this._log = new SoftReference<xLog>(log);
+		return log;
+	}
+
+
+
+}
--- src/com/poixson/utils/NativeUtils.java
+++ src/com/poixson/utils/NativeUtils.java
@@ -7,180 +7,94 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import com.poixson.utils.exceptions.RequiredArgumentException;
+import com.poixson.utils.xLogger.xLevel;
 import com.poixson.utils.xLogger.xLog;
 
 
 public final class NativeUtils {
 	private NativeUtils() {}
 
-
-
-	public static boolean LoadLibrary(final String fileName) {
-		return LoadLibrary(
-			null,
-			fileName
-		);
-	}
-	public static boolean LoadLibrary(
-			final String dirStr, final String fileName) {
-		try {
-			log().finest("Loading library.. {}", fileName);
-			final String pathStr =
-				FileUtils.MergePaths(
-					(Utils.isEmpty(dirStr) ? "." : dirStr),
-					fileName
-				);
-			log().detail("LoadLibrary::pathStr = {}", pathStr);
-			System.load(pathStr);
-		} catch (SecurityException e) {
-			log().trace(e);
-			return false;
-		} catch (UnsatisfiedLinkError e) {
-			log().trace(e);
-			return false;
-		}
-		return true;
-	}
+	private static final int EXTRACT_BUFFER_SIZE = 4096;
 
 
 
-	public static boolean ExtractLibrary(
-			final String outputDir, final String resourcePath,
-			final String fileName, final Class<?> classRef) {
-		if (Utils.isEmpty(fileName)    ) throw new RequiredArgumentException("fileName");
-		if (classRef == null           ) throw new RequiredArgumentException("classRef");
-		final File file =
-			new File(
-				FileUtils.MergePaths(
-					(Utils.isEmpty(outputDir) ? "." : outputDir),
-					fileName
-				)
+	public static void LoadLibrary(final String...filePath)
+			throws SecurityException, UnsatisfiedLinkError {
+		final String pathStr =
+			FileUtils.MergePaths(filePath);
+		if (log().isLoggable(xLevel.DETAIL)) {
+			log().detail(
+				"NativeUtils::LoadLibrary(path={})",
+				pathStr
 			);
-		if (file.exists() && file.isFile()) {
-			log().info("Removing existing library file: {}", fileName);
-			file.delete();
 		}
+		System.load(pathStr);
+	}
+
+
+
+	public static void ExtractLibrary(
+			final String outputDir, final String resourcePath,
+			final String fileName, final Class<?> classRef)
+			throws IOException {
+		if (Utils.isEmpty(fileName)) throw new RequiredArgumentException("fileName");
+		if (classRef == null       ) throw new RequiredArgumentException("classRef");
+		if (log().isLoggable(xLevel.DETAIL)) {
+			log().detail(
+				"NativeUtils::ExtractLibrary(outDir={},resPath={},file={})",
+				outputDir,
+				resourcePath,
+				fileName
+			);
+		}
+		// prepare paths
+		final String resPath =
+			FileUtils.MergePaths(
+				resourcePath,
+				fileName
+			);
+		final String outFilePath =
+			FileUtils.MergePaths(
+				outputDir,
+				fileName
+			);
+		final File outFile = new File(outFilePath);
 		InputStream      in  = null;
 		FileOutputStream out = null;
+		// open resource
+		in = classRef.getResourceAsStream(resPath);
+		if (in == null)
+			throw new IOException("Resource file not found: "+resPath);
+		if (outFile.isFile()) {
+			log().info("Removing existing library file: {}", outFilePath);
+			if (!outFile.delete())
+				throw new IOException("Failed to remove library file: "+outFilePath);
+		}
+		// write file
 		try {
-			final String resPath =
-				FileUtils.MergePaths(
-					(Utils.isEmpty(resourcePath) ? "/" : resourcePath),
-					fileName
-				);
-			in = classRef
-				.getResourceAsStream(
-					resPath
-				);
-			if (in == null)
-				return false;
-			out = new FileOutputStream(file);
-			byte[] buf = new byte[4096];
+			out = new FileOutputStream(outFile);
+			byte[] buf = new byte[EXTRACT_BUFFER_SIZE];
 			while (true) {
 				final int read = in.read(buf);
 				if (read == -1)
 					break;
 				out.write(buf, 0, read);
 			}
-			log().info("Extracted library file: {}", fileName);
+			log().info("Extracted library file: {}", outFilePath);
 		} catch (FileNotFoundException e) {
-			log().trace(e);
-			if (file.isFile()) {
-				file.delete();
-			}
-			return false;
-		} catch (IOException e) {
-			log().trace(e);
-			if (file.isFile()) {
-				file.delete();
-			}
-			return false;
+			throw new IOException("Cannot write to file: "+outFilePath, e);
 		} finally {
 			Utils.safeClose(out);
 			Utils.safeClose(in);
 		}
-		return true;
-	}
-
-
-
-	public static boolean LoadExtractLibrary(
-			final String fileName) {
-		return LoadExtractLibrary(
-			null,
-			null,
-			fileName,
-			null
-		);
-	}
-	public static boolean LoadExtractLibrary(
-			final String fileName, final Class<?> classRef) {
-		return LoadExtractLibrary(
-			null,
-			null,
-			fileName,
-			classRef
-		);
-	}
-	public static boolean LoadExtractLibrary(
-			final String outputDir,
-			final String fileName, final Class<?> classRef) {
-		return LoadExtractLibrary(
-			outputDir,
-			null,
-			fileName,
-			classRef
-		);
-	}
-	public static boolean LoadExtractLibrary(
-			final String outputDir, final String resourcePath,
-			final String fileName, final Class<?> classRef) {
-//TODO: is this useful?
-//String jsscLibPath = System.getProperty("jssc.library.path");
-		final String pathStr =
-			FileUtils.MergePaths(
-				(Utils.isEmpty(outputDir) ? "." : outputDir),
-				fileName
-			);
-		log().detail("LoadExtractLibrary::pathStr = {}", pathStr);
-		final File file = new File(pathStr);
-		// load existing library file
-		if (file.exists() && file.isFile()) {
-			log().finest("Found library: {}", fileName);
-			return LoadLibrary(
-				outputDir,
-				fileName
-			);
-		}
-		// extract library file from jar
-		{
-			final boolean result =
-				ExtractLibrary(
-					outputDir,
-					resourcePath,
-					fileName,
-					classRef
-				);
-			if (!result)
-				return false;
-		}
-		// load extracted library
-		{
-			final boolean result =
-				LoadLibrary(
-					outputDir,
-					fileName
-				);
-			if (!result)
-				return false;
-		}
-		return true;
 	}
 
 
 
 	public static xLog log() {
-		return xLog.getRoot();
+		return
+			xLog.getRoot()
+				.get("LibUtils");
 	}
 
 
