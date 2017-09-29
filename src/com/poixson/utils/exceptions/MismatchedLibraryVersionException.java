5ba4359563d3d94b8a91c4800a62383bae8f7805
lorenzop <lorenzo@poixson.com>
2017-09-29 00:52:39 -0400

    added MismatchedLibraryVersionException exception class

---
+++ src/com/poixson/utils/exceptions/MismatchedLibraryVersionException.java
@@ -0,0 +1,28 @@
+package com.poixson.utils.exceptions;
+
+
+public class MismatchedLibraryVersionException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public final String expectedVersion;
+	public final String libraryVersion;
+
+
+
+	public MismatchedLibraryVersionException(
+			final String expectedVersion, final String libraryVersion) {
+		super(
+			(new StringBuilder())
+				.append("Expected version: ")
+				.append(expectedVersion)
+				.append(" found library version: ")
+				.append(libraryVersion)
+				.toString()
+		);
+		this.expectedVersion = expectedVersion;
+		this.libraryVersion  = libraryVersion;
+	}
+
+
+
+}
