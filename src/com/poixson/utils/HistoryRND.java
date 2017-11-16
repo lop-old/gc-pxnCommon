0ec6dbec6cda6e02e60968d9e423e559e04be1fe
lorenzop <lorenzo@poixson.com>
2017-11-15 19:38:19 -0500

    many big improvements to HistoryRND class

--- src/com/poixson/utils/HistoryRND.java
+++ src/com/poixson/utils/HistoryRND.java
@@ -1,66 +1,131 @@
 package com.poixson.utils;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class HistoryRND {
 
-	private final int min;
-	private final int max;
-	private volatile int last;
-	private final BlockingQueue<Integer> history = new LinkedBlockingQueue<Integer>();
-	private final int historySize;
+	public static final int DEFAULT_MAX_TRIES = 10;
+
+	private int minValue;
+	private int maxValue;
+
+	private final Random gen = new Random(Utils.getSystemMillis());
+
+	private final RollingList<Integer> history;
+	private final AtomicInteger index = new AtomicInteger(0);
+
+	private int maxTries = Integer.MIN_VALUE;
+	private int lastValue = Integer.MIN_VALUE;
 
 
 
-	public HistoryRND(final int min, final int max) {
-		this(min, max, (max - min) / 2);
+	public HistoryRND(final int minValue, final int maxValue) {
+		this(
+			minValue,
+			maxValue,
+			NumberUtils.MinMax(
+				(int)(
+					((double)(maxValue - minValue)) / 1.5
+				),
+				2,
+				1000
+			)
+		);
 	}
-	public HistoryRND(final int min, final int max, final int historySize) {
-		if (min > max) throw new IllegalArgumentException("min must be lower than max!");
-		this.min = min;
-		this.max = max;
-		this.historySize = NumberUtils.MinMax(historySize, 2, (max-min)-1);
-		this.last = min - 1;
+	public HistoryRND(final int minValue, final int maxValue, final int historySize) {
+		if (minValue > maxValue) throw new IllegalArgumentException("minValue must be lower than maxValue!");
+		this.minValue = minValue;
+		this.maxValue = maxValue;
+		final int histSize =
+			NumberUtils.MinMax(
+				historySize,
+				2,
+				(maxValue - minValue) + 1
+			);
+		this.history = new RollingList<Integer>(histSize);
 	}
 
 
 
 	// random number (unique history)
 	public synchronized int RND() {
-		if (this.min == this.max)
-			return this.min;
-		if ((this.max - this.min) == 1) {
-			if (this.last == this.min) {
-				this.last = this.max;
-				return this.max;
+		final int index = this.index.incrementAndGet();
+		final int maxValue  = this.getMaxValue();
+		final int minValue  = this.getMinValue();
+		if (minValue == maxValue)
+			return minValue;
+		if ((maxValue - minValue) == 1) {
+			if (lastValue == minValue) {
+				this.lastValue = maxValue;
+				return maxValue;
 			}
-			this.last = this.min;
-			return this.min;
+			this.lastValue = minValue;
+			return minValue;
 		}
-		int number = 0;
-		int i = 10;
 		// find unique random number
+		int tries = 0;
+		final int maxTries = this.getMaxTries();
 		while (true) {
-			// find a new random number
-			number = NumberUtils.getRandom(this.min, this.max);
-			if (!this.history.contains(new Integer(number))) break;
+			// get a new random number
+			gen.setSeed( Utils.getSystemMillis() + ((long)tries) + ((long)index) );
+			final int number = gen.nextInt(maxValue - minValue) + minValue;
+			if ( ! this.history.contains(new Integer(number)) ) {
+				this.lastValue = number;
+				this.history.add(new Integer(number));
+				return number;
+			}
 			// give up trying
-			i--;
-			if (i < 0) {
-				if (number != this.last)
-					break;
-				if (i < 0-i)
-					break;
+			tries++;
+			if (tries >= maxTries) {
+				if (number != this.lastValue) {
+					this.lastValue = number;
+					return number;
+				}
+				if (tries > maxTries) {
+					return number;
+				}
 			}
 		}
-		this.last = number;
-		this.history.add(new Integer(number));
-		while (this.history.size() > this.historySize) {
-			this.history.remove();
-		}
-		return number;
+	}
+
+
+
+	// min value
+	public HistoryRND setMinValue(final int minValue) {
+		this.minValue = minValue;
+		return this;
+	}
+	public int getMinValue() {
+		return this.minValue;
+	}
+
+
+
+	// max value
+	public HistoryRND setMaxValue(final int maxValue) {
+		this.maxValue = maxValue;
+		return this;
+	}
+	public int getMaxValue() {
+		return this.maxValue;
+	}
+
+
+
+	// max tries to find a unique number
+	public HistoryRND setMaxTries(final int maxTries) {
+		this.maxTries = maxTries;
+		return this;
+	}
+	public int getMaxTries() {
+		final int maxTries = this.maxTries;
+		return (
+			maxTries == Integer.MIN_VALUE
+			? DEFAULT_MAX_TRIES
+			: maxTries
+		);
 	}
 
 
