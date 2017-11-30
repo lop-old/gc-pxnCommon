13b02dc4cebbf1b7a1b43f6c670e99e14a2adc4e
lorenzop <lorenzo@poixson.com>
2017-11-29 20:55:08 -0500

    added explicit functions to xCloseable and xCloseableMany interfaces

--- src/com/poixson/utils/xCloseable.java
+++ src/com/poixson/utils/xCloseable.java
@@ -1,10 +1,14 @@
 package com.poixson.utils;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 
 public interface xCloseable extends Closeable {
 
+
+	@Override
+	public void close() throws IOException;
 
 	public boolean isClosed();
 
--- src/com/poixson/utils/xCloseableMany.java
+++ src/com/poixson/utils/xCloseableMany.java
@@ -1,10 +1,17 @@
 package com.poixson.utils;
 
+import java.io.IOException;
+
 
 public interface xCloseableMany extends xCloseable {
 
 
+	@Override
+	public void close() throws IOException;
 	public void CloseAll();
+
+	@Override
+	public boolean isClosed();
 
 
 }
