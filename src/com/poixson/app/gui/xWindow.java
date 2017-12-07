6ba91eed8b73a1f6a038f639a62733f024f4bdd8
lorenzop <lorenzo@poixson.com>
2017-12-07 14:27:49 -0500

    cleaned up xWindow class

--- src/com/poixson/app/gui/xWindow.java
+++ src/com/poixson/app/gui/xWindow.java
@@ -3,58 +3,75 @@ package com.poixson.app.gui;
 import java.awt.HeadlessException;
 import java.awt.LayoutManager;
 import java.io.Closeable;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.swing.JFrame;
 
-import com.poixson.commonapp.gui.annotations.xWindowProperties;
-import com.poixson.commonapp.gui.remapped.RemappedWindowAdapter;
-import com.poixson.commonjava.Utils.utils;
-import com.poixson.commonjava.Utils.utilsString;
-import com.poixson.commonjava.xLogger.xLog;
+import com.poixson.app.gui.annotations.xWindowProperties;
+import com.poixson.app.gui.remapped.RemappedWindowAdapter;
+import com.poixson.utils.StringUtils;
+import com.poixson.utils.Utils;
+import com.poixson.utils.xLogger.xLog;
 
 
 public abstract class xWindow extends JFrame implements Closeable {
 	private static final long serialVersionUID = 1L;
 	private static final String LOG_NAME = "GUI";
 
-	private static final ConcurrentMap<String, xWindow> windows =
+	private static final ConcurrentMap<String, xWindow> allWindows =
 			new ConcurrentHashMap<String, xWindow>();
 
-	public final String windowName;
-	protected final AtomicBoolean closing     = new AtomicBoolean(false);
-	private final   AtomicBoolean closeHooked = new AtomicBoolean(false);
+	private final String windowKey;
+
+	private final AtomicBoolean closing     = new AtomicBoolean(false);
+	private final AtomicBoolean closeHooked = new AtomicBoolean(false);
 
 
 
 	// new window instance
 	public xWindow() throws HeadlessException {
-		synchronized(windows) {
-			// unique window name
-			this.windowName =
-				utilsString.ensureUnique(
-					utilsString.getLastPart(
-						".",
-						this.getClass().getName()
-					),
-					windows.keySet()
+		// find a unique name
+		{
+			final String className =
+				StringUtils.PeekLastPart(
+					this.getClass().getName(),
+					'.'
 				);
-			windows.put(this.windowName, this);
+			if (Utils.isEmpty(className)) throw new RuntimeException("Failed to detect window class name");
+			final Set<String> winKeys = allWindows.keySet();
+			String last = null;
+			while (true) {
+				final String name =
+					StringUtils.ForceUnique(
+						className,
+						winKeys
+					);
+				if (StringUtils.StrEquals(name, last))
+					throw new RuntimeException("Detected duplicate window key when attempting to generate a unique key!");
+				if (allWindows.putIfAbsent(name, this) == null) {
+					this.windowKey = name;
+					log().fine("New window created: {}", name);
+					break;
+				}
+				last = name;
+			}
 		}
-		log().finer("New window created: "+this.windowName);
 		// annotations
-		final xWindowProperties props = this.getClass().getAnnotation(xWindowProperties.class);
+		final xWindowProperties props =
+			this.getClass().getAnnotation(xWindowProperties.class);
 		if (props != null) {
 			// window title
 			final String title = props.title();
-			if (utils.notEmpty(title)) {
+			if (Utils.notEmpty(title)) {
 				this.setTitle(title);
 			}
 			// resizable
 			this.setResizable(props.resizable());
 		}
+		this.registerCloseHook();
 	}
 	public xWindow(final String title) throws HeadlessException {
 		this();
@@ -67,29 +84,16 @@ public abstract class xWindow extends JFrame implements Closeable {
 
 
 
-	public void autoHeight(final int width) {
-		if (guiUtils.forceDispatchThread(this, "autoHeight"))
-			return;
-		this.pack();
-		this.setSize(width, this.getHeight());
+	@Override
+	public void close() {
+		if (guiUtils.forceDispatchThread(this, "close")) return;
+		// only close once
+		if (!this.closing.compareAndSet(false, true))    return;
+		// close window
+		log().fine("Closing window: {}", this.getWindowKey());
+		this.dispose();
 	}
-
-
-
-	// show window
-	public void Show() {
-		if (guiUtils.forceDispatchThread(this, "Show"))
-			return;
-		this.setVisible(true);
-		if (!this.isFocused()) {
-			this.setVisible(true);
-			this.requestFocus();
-		}
-	}
-
-
-
-	protected void registerCloseHook() {
+	private void registerCloseHook() {
 		if (!this.closeHooked.compareAndSet(false, true))            return;
 		if (guiUtils.forceDispatchThread(this, "registerCloseHook")) return;
 		// window close event listener
@@ -100,20 +104,39 @@ public abstract class xWindow extends JFrame implements Closeable {
 			)
 		);
 	}
-	@Override
-	public void close() {
-		if (guiUtils.forceDispatchThread(this, "close")) return;
-		if (!this.closing.compareAndSet(false, true))    return;
-		// close window
-		log().fine("Closing window: "+this.windowName);
-		this.dispose();
+
+
+
+	public String getWindowKey() {
+		return this.windowKey;
+	}
+
+
+
+	public void autoHeight(final int width) {
+		if (guiUtils.forceDispatchThread(this, "autoHeight", Integer.valueOf(width)))
+			return;
+		this.pack();
+		this.setSize(width, this.getHeight());
+	}
+
+
+
+	public void showFocused() {
+		if (guiUtils.forceDispatchThread(this, "showFocused"))
+			return;
+		this.setVisible(true);
+		if (!this.isFocused()) {
+			this.requestFocus();
+		}
 	}
 
 
 
 	// logger
 	public static xLog log() {
-		return xLog.getRoot(LOG_NAME);
+		return xLog.getRoot()
+				.get(LOG_NAME);
 	}
 
 
