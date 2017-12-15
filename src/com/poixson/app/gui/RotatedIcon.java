17e9e6aa28190f88ddb8cae8fc66f4e1307d5390
lorenzop <lorenzo@poixson.com>
2017-12-15 10:36:25 -0500

    added TextIcon and RotatedIcon classes

---
+++ src/com/poixson/app/gui/RotatedIcon.java
@@ -0,0 +1,300 @@
+package com.poixson.app.gui;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.Icon;
+
+
+/**
+ * The RotatedIcon allows you to change the orientation of an Icon by
+ * rotating the Icon before it is painted. This class supports the following
+ * orientations:
+ *
+ * <ul>
+ *   <li>DOWN - rotated 90 degrees
+ *   <li>UP (default) - rotated -90 degrees
+ *   <li>UPSIDE_DOWN - rotated 180 degrees
+ *   <li>ABOUT_CENTER - the icon is rotated by the specified degrees about its center.
+ * </ul>
+ *
+ * https://tips4java.wordpress.com/2009/04/06/rotated-icon/
+ */
+public class RotatedIcon implements Icon {
+
+	public enum Rotate {
+		DOWN,
+		UP,
+		UPSIDE_DOWN,
+		ABOUT_CENTER
+	}
+
+	private final Icon icon;
+
+	private final Rotate rotate;
+
+	private double degrees;
+	private boolean circularIcon;
+
+
+
+	/**
+	 * Convenience constructor to create a RotatedIcon that is rotated DOWN.
+	 *
+	 * @param icon  the Icon to rotate
+	 */
+	public RotatedIcon(final Icon icon) {
+		this(icon, Rotate.UP);
+	}
+	/**
+	 * Create a RotatedIcon
+	 *
+	 * @param icon	the Icon to rotate
+	 * @param rotate  the direction of rotation
+	 */
+	public RotatedIcon(final Icon icon, final Rotate rotate) {
+		this.icon = icon;
+		this.rotate = rotate;
+	}
+	/**
+	 * Create a RotatedIcon. The icon will rotate about its center. This
+	 * constructor will automatically set the Rotate enum to ABOUT_CENTER.
+	 *
+	 * @param icon	the Icon to rotate
+	 * @param degrees   the degrees of rotation
+	 */
+	public RotatedIcon(final Icon icon, final double degrees) {
+		this(icon, degrees, false);
+	}
+	/**
+	 * Create a RotatedIcon. The icon will rotate about its center. This
+	 * constructor will automatically set the Rotate enum to ABOUT_CENTER.
+	 *
+	 * @param icon	the Icon to rotate
+	 * @param degrees   the degrees of rotation
+	 * @param circularIcon treat the icon as circular so its size doesn't change
+	 */
+	public RotatedIcon(final Icon icon,
+			final double degrees, final boolean circularIcon) {
+		this(icon, Rotate.ABOUT_CENTER);
+		this.setDegrees(degrees);
+		this.setCircularIcon(circularIcon);
+	}
+
+
+
+	/**
+	 * Gets the Icon to be rotated
+	 *
+	 * @return the Icon to be rotated
+	 */
+	public Icon getIcon() {
+		return this.icon;
+	}
+
+
+
+	/**
+	 * Gets the Rotate enum which indicates the direction of rotation
+	 *
+	 * @return the Rotate enum
+	 */
+	public Rotate getRotate() {
+		return this.rotate;
+	}
+	/**
+	 * Gets the degrees of rotation. Only used for Rotate.ABOUT_CENTER.
+	 *
+	 * @return the degrees of rotation
+	 */
+	public double getDegrees() {
+		return this.degrees;
+	}
+	/**
+	 * Set the degrees of rotation. Only used for Rotate.ABOUT_CENTER.
+	 * This method only sets the degress of rotation, it will not cause
+	 * the Icon to be repainted. You must invoke repaint() on any
+	 * component using this icon for it to be repainted.
+	 *
+	 * @param degrees the degrees of rotation
+	 */
+	public void setDegrees(final double degrees) {
+		this.degrees = degrees;
+	}
+
+
+
+	/**
+	 * Is the image circular or rectangular? Only used for Rotate.ABOUT_CENTER.
+	 * When true, the icon width/height will not change as the Icon is rotated.
+	 *
+	 * @return true for a circular Icon, false otherwise
+	 */
+	public boolean isCircularIcon() {
+		return this.circularIcon;
+	}
+	/**
+	 * Set the Icon as circular or rectangular. Only used for Rotate.ABOUT_CENTER.
+	 * When true, the icon width/height will not change as the Icon is rotated.
+	 *
+	 * @param true for a circular Icon, false otherwise
+	 */
+	public void setCircularIcon(final boolean circularIcon) {
+		this.circularIcon = circularIcon;
+	}
+
+
+
+	//
+	// Implement the Icon Interface
+	//
+
+
+
+	/**
+	 * Gets the width of this icon.
+	 *
+	 * @return the width of the icon in pixels.
+	 */
+	@Override
+	public int getIconWidth() {
+		if (this.rotate == Rotate.ABOUT_CENTER) {
+			if (this.circularIcon) {
+				return this.icon.getIconWidth();
+			}
+			final double rads = Math.toRadians(this.degrees);
+			final double sin = Math.abs( Math.sin(rads) );
+			final double cos = Math.abs( Math.cos(rads) );
+			final int width =
+				(int) Math.floor(
+					(this.icon.getIconWidth()  * cos) +
+					(this.icon.getIconHeight() * sin)
+				);
+			return width;
+		}
+		if (this.rotate == Rotate.UPSIDE_DOWN) {
+			return this.icon.getIconWidth();
+		}
+		return this.icon.getIconHeight();
+	}
+
+
+
+	/**
+	 * Gets the height of this icon.
+	 *
+	 * @return the height of the icon in pixels.
+	 */
+	@Override
+	public int getIconHeight() {
+		if (this.rotate == Rotate.ABOUT_CENTER) {
+			if (this.circularIcon) {
+				return this.icon.getIconHeight();
+			}
+			final double rads = Math.toRadians(this.degrees);
+			final double sin = Math.abs( Math.sin(rads) );
+			final double cos = Math.abs( Math.cos(rads) );
+			final int height =
+				(int) Math.floor(
+					(this.icon.getIconHeight() * cos) +
+					(this.icon.getIconWidth() * sin)
+				);
+			return height;
+		}
+		if (this.rotate == Rotate.UPSIDE_DOWN) {
+			return this.icon.getIconHeight();
+		}
+		return this.icon.getIconWidth();
+	}
+
+
+
+	/**
+	 * Paint the icons of this compound icon at the specified location
+	 *
+	 * @param c The component on which the icon is painted
+	 * @param g the graphics context
+	 * @param x the X coordinate of the icon's top-left corner
+	 * @param y the Y coordinate of the icon's top-left corner
+	 */
+	@Override
+	public void paintIcon(final Component c, final Graphics g, final int x, final int y) {
+		final Graphics2D g2d = (Graphics2D) g.create();
+		final int cWidth  = this.icon.getIconWidth() / 2;
+		final int cHeight = this.icon.getIconHeight() / 2;
+		final int xAdjustment = (this.icon.getIconWidth() % 2) == 0 ? 0 : -1;
+		final int yAdjustment = (this.icon.getIconHeight() % 2) == 0 ? 0 : -1;
+		if (this.rotate == Rotate.DOWN) {
+			g2d.translate(
+				x + cHeight,
+				y + cWidth
+			);
+			g2d.rotate( Math.toRadians(90.0) );
+			this.icon.paintIcon(
+				c,
+				g2d,
+				0 - cWidth,
+				yAdjustment - cHeight
+			);
+		} else
+		if (this.rotate == Rotate.UP) {
+			g2d.translate(
+				x + cHeight,
+				y + cWidth
+			);
+			g2d.rotate( Math.toRadians(270.0) );
+			this.icon.paintIcon(
+				c,
+				g2d,
+				xAdjustment - cWidth,
+				0 - cHeight
+			);
+		} else
+		if (this.rotate == Rotate.UPSIDE_DOWN) {
+			g2d.translate(
+				x + cWidth,
+				y + cHeight
+			);
+			g2d.rotate( Math.toRadians(180.0) );
+			this.icon.paintIcon(
+				c,
+				g2d,
+				xAdjustment - cWidth,
+				yAdjustment - cHeight
+			);
+		} else
+		if (this.rotate == Rotate.ABOUT_CENTER) {
+			g2d.setRenderingHint(
+				RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON
+			);
+			g2d.setClip(
+				x,
+				y,
+				this.getIconWidth(),
+				this.getIconHeight()
+			);
+			g2d.translate(
+				(this.getIconWidth()  - this.icon.getIconWidth())  / 2,
+				(this.getIconHeight() - this.icon.getIconHeight()) / 2
+			);
+			g2d.rotate(
+				Math.toRadians(this.degrees),
+				x + cWidth,
+				y + cHeight
+			);
+			this.icon.paintIcon(
+				c,
+				g2d,
+				x,
+				y
+			);
+		}
+		g2d.dispose();
+	}
+
+
+
+}
---
+++ src/com/poixson/app/gui/TextIcon.java
@@ -0,0 +1,308 @@
+package com.poixson.app.gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.Icon;
+import javax.swing.JComponent;
+
+
+/**
+ * The TextIcon will paint a String of text as an Icon. The Icon
+ * can be used by any Swing component that supports icons.
+ *
+ * TextIcon supports two different layout styles:
+ * <ul>
+ * <li>Horizontally - does normal rendering of the text by using the
+ *   Graphics.drawString(...) method
+ * <li>Vertically - Each character is displayed on a separate line
+ * </ul>
+ *
+ * TextIcon was designed to be rendered on a specific JComponent as it
+ * requires FontMetrics information in order to calculate its size and to do
+ * the rendering. Therefore, it should only be added to component it was
+ * created for.
+ *
+ * By default the text will be rendered using the Font and foreground color
+ * of its associated component. However, this class does allow you to override
+ * these properties. Also starting in JDK6 the desktop renderering hints will
+ * be used to renderer the text. For versions not supporting the rendering
+ * hints antialiasing will be turned on.
+ *
+ * https://tips4java.wordpress.com/2009/04/02/text-icon/
+ */
+public class TextIcon implements Icon, PropertyChangeListener {
+
+	public enum Layout {
+		HORIZONTAL,
+		VERTICAL
+	}
+
+	private JComponent component;
+
+	private Layout layout;
+
+	private String text;
+
+	private Font font;
+
+	private Color foreground;
+
+	private int padding;
+
+	// Used for the implementation of Icon interface
+	private int iconWidth;
+	private int iconHeight;
+
+	// Used for Layout.VERTICAL to save reparsing the text every time the icon is repainted
+	private String[] strings;
+	private int[] stringWidths;
+
+
+
+	/**
+	 * Convenience constructor to create a TextIcon with a HORIZONTAL layout.
+	 *
+	 * @param component  the component to which the icon will be added
+	 * @param text       the text to be rendered on the Icon
+	 */
+	public TextIcon(final JComponent component, final String text) {
+		this(component, text, Layout.HORIZONTAL);
+	}
+	/**
+	 * Create a TextIcon specifying all the properties.
+	 *
+	 * @param component  the component to which the icon will be added
+	 * @param text       the text to be rendered on the Icon
+	 * @param layout     specify the layout of the text. Must be one of
+	 *                   the Layout enums: HORIZONTAL or VERTICAL
+	 */
+	public TextIcon(final JComponent component,
+			final String text, final Layout layout) {
+		this.component = component;
+		this.layout = layout;
+		this.setText(text);
+		component.addPropertyChangeListener("font", this);
+	}
+
+
+
+	/**
+	 * Get the Layout enum
+	 *
+	 * @return the Layout enum
+	 */
+	public Layout getLayout() {
+		return this.layout;
+	}
+
+
+
+	/**
+	 * Get the text String that will be rendered on the Icon
+	 *
+	 * @return the text of the Icon
+	 */
+	public String getText() {
+		return this.text;
+	}
+	/**
+	 * Set the text to be rendered on the Icon
+	 *
+	 * @param text  the text to be rendered on the Icon
+	 */
+	public void setText(final String text) {
+		this.text = text;
+		calculateIconDimensions();
+	}
+
+
+
+	/**
+	 * Get the Font used to render the text. This will default to the Font
+	 * of the component unless the Font has been overridden by using the
+	 * setFont() method.
+	 *
+	 * @return the Font used to render the text
+	 */
+	public Font getFont() {
+		if (this.font == null)
+			return this.component.getFont();
+		return this.font;
+	}
+	/**
+	 * Set the Font to be used for rendering the text
+	 *
+	 * @param font  the Font to be used for rendering the text
+	 */
+	public void setFont(final Font font) {
+		this.font = font;
+		calculateIconDimensions();
+	}
+
+
+
+	/**
+	 * Get the foreground Color used to render the text. This will default to
+	 * the foreground Color of the component unless the foreground Color has
+	 * been overridden by using the setForeground() method.
+	 *
+	 * @return the Color used to render the text
+	 */
+	public Color getForeground() {
+		if (this.foreground == null)
+			return this.component.getForeground();
+		return this.foreground;
+	}
+	/**
+	 * Set the foreground Color to be used for rendering the text
+	 *
+	 * @param foreground  the foreground Color to be used for rendering the text
+	 */
+	public void setForeground(final Color foreground) {
+		this.foreground = foreground;
+		this.component.repaint();
+	}
+
+
+
+	/**
+	 * Get the padding used when rendering the text
+	 *
+	 * @return the padding specified in pixels
+	 */
+	public int getPadding() {
+		return this.padding;
+	}
+	/**
+	 * By default the size of the Icon is based on the size of the rendered
+	 * text. You can specify some padding to be added to the start and end
+	 * of the text when it is rendered.
+	 *
+	 * @param padding  the padding amount in pixels
+	 */
+	public void setPadding(final int padding) {
+		this.padding = padding;
+		this.calculateIconDimensions();
+	}
+
+
+
+	/**
+	 * Calculate the size of the Icon using the FontMetrics of the Font.
+	 */
+	private void calculateIconDimensions() {
+		final Font font = this.getFont();
+		final FontMetrics fm = this.component.getFontMetrics( font );
+		if (this.layout == Layout.HORIZONTAL) {
+			this.iconWidth  = fm.stringWidth(this.text) + (this.padding * 2);
+			this.iconHeight = fm.getHeight();
+		} else
+		if (this.layout == Layout.VERTICAL) {
+			int maxWidth = 0;
+			this.strings = new String[this.text.length()];
+			this.stringWidths = new int[this.text.length()];
+			// Find the widest character in the text string
+			for (int i=0; i < this.text.length(); i++) {
+				this.strings[i] = this.text.substring(i, i + 1);
+				this.stringWidths[i] = fm.stringWidth( this.strings[i] );
+				maxWidth = Math.max(maxWidth, this.stringWidths[i]);
+			}
+			// Add a minimum of 2 extra pixels, plus the leading value,
+			// on each side of the character.
+			this.iconWidth = maxWidth + ((fm.getLeading() + 2) * 2);
+			// Decrease then normal gap betweens lines of text by taking into
+			// account the descent.
+			this.iconHeight = (fm.getHeight() - fm.getDescent()) * this.text.length();
+			this.iconHeight += this.padding * 2;
+		}
+		this.component.revalidate();
+	}
+
+
+
+	//
+	// Implement the Icon Interface
+	//
+
+
+
+	/**
+	 * Gets the width of this icon.
+	 *
+	 * @return the width of the icon in pixels.
+	 */
+	@Override
+	public int getIconWidth() {
+		return this.iconWidth;
+	}
+	/**
+	 * Gets the height of this icon.
+	 *
+	 * @return the height of the icon in pixels.
+	 */
+	@Override
+	public int getIconHeight() {
+		return this.iconHeight;
+	}
+
+
+
+	/**
+	 * Paint the icons of this compound icon at the specified location
+	 *
+	 * @param c The component to which the icon is added
+	 * @param g the graphics context
+	 * @param x the X coordinate of the icon's top-left corner
+	 * @param y the Y coordinate of the icon's top-left corner
+	 */
+	@Override
+	public void paintIcon(final Component c, final Graphics g, final int x, final int y) {
+		final Graphics2D g2d = (Graphics2D) g.create();
+		g2d.setRenderingHint(
+			RenderingHints.KEY_TEXT_ANTIALIASING,
+			RenderingHints.VALUE_TEXT_ANTIALIAS_ON
+		);
+		g2d.setFont(this.getFont() );
+		g2d.setColor(this.getForeground());
+		final FontMetrics fm = g2d.getFontMetrics();
+		if (this.layout == Layout.HORIZONTAL) {
+			g2d.translate(x, y + fm.getAscent());
+			g2d.drawString(this.text, this.padding, 0);
+		} else
+		if (this.layout == Layout.VERTICAL) {
+			int offsetY = fm.getAscent() - fm.getDescent() + this.padding;
+			final int incrementY = fm.getHeight() - fm.getDescent();
+			for (int i=0; i < this.text.length(); i++) {
+				final int offsetX = Math.round((getIconWidth() - this.stringWidths[i]) / 2.0f);
+				g2d.drawString(this.strings[i], x + offsetX, y + offsetY);
+				offsetY += incrementY;
+			}
+		}
+		g2d.dispose();
+	}
+
+
+
+	//
+	// Implement the PropertyChangeListener interface
+	//
+
+
+
+	public void propertyChange(final PropertyChangeEvent event) {
+		if (this.font == null) {
+				calculateIconDimensions();
+		}
+	}
+
+
+
+}
