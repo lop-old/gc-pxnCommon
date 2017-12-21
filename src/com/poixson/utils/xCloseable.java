a6f7665acfd4fe9acf97318868d565a9caa1c35d
lorenzop <lorenzo@poixson.com>
2017-12-21 00:29:06 -0500

    removed unnecessary overrides in xCloseable interfaces

--- src/com/poixson/utils/xCloseable.java
+++ src/com/poixson/utils/xCloseable.java
@@ -1,14 +1,10 @@
 package com.poixson.utils;
 
 import java.io.Closeable;
-import java.io.IOException;
 
 
 public interface xCloseable extends Closeable {
 
-
-	@Override
-	public void close() throws IOException;
 
 	public boolean isClosed();
 
--- src/com/poixson/utils/xCloseableMany.java
+++ src/com/poixson/utils/xCloseableMany.java
@@ -1,17 +1,10 @@
 package com.poixson.utils;
 
-import java.io.IOException;
-
 
 public interface xCloseableMany extends xCloseable {
 
 
-	@Override
-	public void close() throws IOException;
 	public void closeAll();
-
-	@Override
-	public boolean isClosed();
 
 
 }
