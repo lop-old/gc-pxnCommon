16109065ec4b7de6053d3efe1d133296db8a50b3
lorenzop <lorenzo@poixson.com>
2018-01-04 22:13:21 -0500

    added new xPluginDefines class

--- app/src/com/poixson/app/appDefines.java
+++ app/src/com/poixson/app/appDefines.java
@@ -15,16 +15,4 @@ public final class appDefines {
 
 
 
-	// plugins
-	public static final String DEFAULT_PLUGINS_DIR      = "plugins/";
-	public static final String DEFAULT_PLUGIN_CLASS_KEY = "Main Class";
-
-	// plugin.yml keys
-	public static final String PLUGIN_NAME    = "Plugin Name";
-	public static final String PLUGIN_VERSION = "Plugin Version";
-	public static final String PLUGIN_AUTHOR  = "Plugin Author";
-	public static final String PLUGIN_WEBSITE = "Plugin Website";
-
-
-
 }
---
+++ plugin/src/com/poixson/tools/plugin/xPluginDefines.java
@@ -0,0 +1,20 @@
+package com.poixson.tools.plugin;
+
+
+public final class xPluginDefines {
+	private xPluginDefines() {}
+
+
+
+	public static final String DEFAULT_PLUGINS_DIR      = "plugins/";
+	public static final String DEFAULT_PLUGIN_CLASS_KEY = "Main Class";
+
+	// plugin.yml keys
+	public static final String PLUGIN_NAME    = "Plugin Name";
+	public static final String PLUGIN_VERSION = "Plugin Version";
+	public static final String PLUGIN_AUTHOR  = "Plugin Author";
+	public static final String PLUGIN_WEBSITE = "Plugin Website";
+
+
+
+}
