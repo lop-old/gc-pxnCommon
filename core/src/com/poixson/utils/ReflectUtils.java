e05d316f342ae5065589f78667445f475138f2ec
lorenzop <lorenzo@poixson.com>
2018-01-10 12:23:29 -0500

    removed some old code from ReflectUtils class

--- core/src/com/poixson/utils/ReflectUtils.java
+++ core/src/com/poixson/utils/ReflectUtils.java
@@ -33,48 +33,6 @@ public final class ReflectUtils {
 					methodName,
 					ArgsToClasses(args)
 			);
-//TODO: remove this if not needed
-//			switch (args.length){
-//			case 0:
-//				return clss.getMethod(
-//					methodName
-//				);
-//			case 1:
-//				return clss.getMethod(
-//					methodName,
-//					args[0].getClass()
-//				);
-//			case 2:
-//				return clss.getMethod(
-//					methodName,
-//					args[0].getClass(), args[1].getClass()
-//				);
-//			case 3:
-//				return clss.getMethod(
-//					methodName,
-//					args[0].getClass(), args[1].getClass(), args[2].getClass()
-//				);
-//			case 4:
-//				return clss.getMethod(
-//					methodName,
-//					args[0].getClass(), args[1].getClass(),
-//					args[2].getClass(), args[3].getClass()
-//				);
-//			case 5:
-//				return clss.getMethod(
-//					methodName,
-//					args[0].getClass(), args[1].getClass(), args[2].getClass(),
-//					args[3].getClass(), args[4].getClass()
-//				);
-//			case 6:
-//				return clss.getMethod(
-//					methodName,
-//					args[0].getClass(), args[1].getClass(), args[2].getClass(),
-//					args[3].getClass(), args[4].getClass(), args[5].getClass()
-//				);
-//			default:
-//				throw new IllegalArgumentException("Too many arguments");
-//			}
 		} catch (NoSuchMethodException
 				| SecurityException e) {
 			throw new IllegalArgumentException("Invalid method: "+methodName, e);
@@ -97,48 +55,6 @@ public final class ReflectUtils {
 					container,
 					(Object[]) ArgsToClasses(args)
 			);
-//TODO: remove this if not needed
-//			switch (args.length) {
-//			case 0:
-//				return method.invoke(
-//						container
-//				);
-//			case 1:
-//				return method.invoke(
-//						container,
-//						args[0]
-//				);
-//			case 2:
-//				return method.invoke(
-//						container,
-//						args[0], args[1]
-//				);
-//			case 3:
-//				return method.invoke(
-//						container,
-//						args[0], args[1], args[2]
-//				);
-//			case 4:
-//				return method.invoke(
-//						container,
-//						args[0], args[1],
-//						args[2], args[3]
-//				);
-//			case 5:
-//				return method.invoke(
-//						container,
-//						args[0], args[1], args[2],
-//						args[3], args[4]
-//				);
-//			case 6:
-//				return method.invoke(
-//						container,
-//						args[0], args[1], args[2],
-//						args[3], args[4], args[5]
-//				);
-//			default:
-//				throw new IllegalArgumentException("Too many arguments");
-//			}
 		} catch (IllegalAccessException
 				| IllegalArgumentException
 				| InvocationTargetException e) {
