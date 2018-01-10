0002f9fe53a55a890dbc326511c424fd2e00679a
lorenzop <lorenzo@poixson.com>
2018-01-10 14:56:59 -0500

    improved UnmodifiableObjectException to allow direct new class() call

--- core/src/com/poixson/exceptions/UnmodifiableObjectException.java
+++ core/src/com/poixson/exceptions/UnmodifiableObjectException.java
@@ -1,6 +1,10 @@
 package com.poixson.exceptions;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 import com.poixson.tools.byref.StringRef;
+import com.poixson.utils.StringUtils;
 
 
 public class UnmodifiableObjectException extends UnsupportedOperationException {
@@ -8,22 +12,38 @@ public class UnmodifiableObjectException extends UnsupportedOperationException {
 
 
 
-	public static UnmodifiableObjectException getNew() {
-		final int index = 1;
+	private static String getMsg() {
 		final Exception eTemp = new Exception();
 		final StackTraceElement[] trace = eTemp.getStackTrace();
-		final String parentClassName =
-			StringRef.getNew(trace[index].getClassName())
-				.peekLastPart('.');
-		final String parentMethodName = trace[index].getMethodName();
-		final StringBuilder msg =
-			(new StringBuilder())
-				.append( "Object cannot be modified! " )
-				.append( parentClassName               )
-				.append( "->"                          )
-				.append( parentMethodName              )
-				.append( "()"                          );
-		return new UnmodifiableObjectException(msg.toString());
+		final Iterator<StackTraceElement> it =
+				Arrays.asList(trace).iterator();
+		// find calling class
+		while (it.hasNext()) {
+			final StackTraceElement e = it.next();
+			final String className = e.getClassName();
+			if (!className.endsWith("UnmodifiableObjectException")) {
+				final String parentClassName =
+					StringRef.getNew(className)
+						.peekLastPart('.');
+				final String parentMethodName =
+					StringUtils.Trim(e.getMethodName(), '<', '>');
+				return
+					(new StringBuilder())
+						.append( "Object cannot be modified! " )
+						.append( parentClassName               )
+						.append( "->"                          )
+						.append( parentMethodName              )
+						.append( "()"                          )
+						.toString();
+			}
+		}
+		return null;
+	}
+
+
+
+	public UnmodifiableObjectException() {
+		super(getMsg());
 	}
 	public UnmodifiableObjectException(final String msg) {
 		super(msg);
