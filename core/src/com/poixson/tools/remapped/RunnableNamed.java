12111fafd8ea6534d2af984022df63395f2cd7b1
lorenzop <lorenzo@poixson.com>
2018-01-21 12:45:07 -0500

    added GetName() static functions to RunnableNamed interface

--- core/src/com/poixson/tools/remapped/RunnableNamed.java
+++ core/src/com/poixson/tools/remapped/RunnableNamed.java
@@ -1,12 +1,33 @@
 package com.poixson.tools.remapped;
 
+import java.util.concurrent.Callable;
+
 
 public interface RunnableNamed extends Runnable {
+
 
 
 	public String getTaskName();
 	public void setTaskName(final String name);
 	public boolean taskNameEquals(final String name);
+
+
+
+	public static String GetName(final Runnable run) {
+		if (run == null)
+			return null;
+		if (run instanceof RunnableNamed)
+			return ((RunnableNamed) run).getTaskName();
+		return null;
+	}
+	public static String GetName(final Callable<?> call) {
+		if (call == null)
+			return null;
+		if (call instanceof RunnableNamed)
+			return ((RunnableNamed) call).getTaskName();
+		return null;
+	}
+
 
 
 }
