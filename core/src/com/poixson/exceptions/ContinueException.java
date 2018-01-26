439c4267181370d1fecfaf9699ba3800c6bb5f56
lorenzop <lorenzo@poixson.com>
2018-01-26 11:52:40 -0500

    added ContinueException

---
+++ core/src/com/poixson/exceptions/ContinueException.java
@@ -0,0 +1,14 @@
+package com.poixson.exceptions;
+
+
+public class ContinueException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+
+
+	public ContinueException() {
+	}
+
+
+
+}
