f5c2ac9b0b17e9296f05b1b859bfb0ca2859dd82
lorenzop <lorenzo@poixson.com>
2018-02-01 16:51:15 -0500

    added more ReplaceTags() functions to StringAdvUtils class

--- core/src/com/poixson/utils/StringAdvUtils.java
+++ core/src/com/poixson/utils/StringAdvUtils.java
@@ -1,7 +1,10 @@
 package com.poixson.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import com.poixson.tools.byref.StringRef;
 
@@ -106,6 +109,243 @@ public final class StringAdvUtils {
 				.append(str);
 		} // end ARG_LOOP
 		return result.toString();
+	}
+
+
+
+	// replace {key} tags
+	public static String ReplaceTags(final String msg, final Map<String, Object> args) {
+		if (Utils.isEmpty(msg))
+			return msg;
+		final StringBuilder result = new StringBuilder(msg);
+		final List<String> extras = new ArrayList<String>(0);
+		//ARG_LOOP:
+		for (final String key : args.keySet()) {
+			final Object obj = args.get(key);
+			final String str = (
+				obj == null
+				? "<null>"
+				: StringUtils.toString(obj)
+			);
+			// {key}
+			final String tag =
+				(new StringBuilder())
+					.append('{')
+					.append(key)
+					.append('}')
+					.toString();
+			boolean found = false;
+			REPLACE_LOOP:
+			while (true) {
+				final int pos = result.indexOf(tag);
+				if (pos == -1)
+					break REPLACE_LOOP;
+				result.replace(
+					pos,
+					pos + tag.length(),
+					str
+				);
+				found = true;
+			} // end REPLACE_LOOP
+			// append
+			if (!found)
+				extras.add(str);
+		} // end ARG_LOOP
+		// check for {} tags
+		if (extras.size() > 0) {
+			final Iterator<String> it = extras.iterator();
+			ARG_LOOP:
+			while (it.hasNext()) {
+				final String arg = it.next();
+				final String str = (
+					arg == null
+					? "<null>"
+					: StringUtils.toString(arg)
+				);
+				// {}
+				final int pos = result.indexOf("{}");
+				if (pos == -1)
+					break ARG_LOOP;
+				result.replace(
+					pos,
+					pos + 2,
+					str
+				);
+				it.remove();
+			} // end ARG_LOOP
+		}
+		// append the rest
+		if (extras.size() > 0) {
+			for (final String str : extras) {
+				result
+					.append(' ')
+					.append(str);
+			}
+		}
+		return result.toString();
+	}
+
+
+
+	// replace {} or {#} tags (in multiple lines)
+	public static String[] ReplaceTags(final String[] msgs, final Object... args) {
+		if (Utils.isEmpty(msgs))
+			return msgs;
+		String[] result = Arrays.copyOf(msgs, msgs.length);
+		final List<String> extras = new ArrayList<String>(0);
+		//ARG_LOOP:
+		for (int argIndex=0; argIndex<args.length; argIndex++) {
+			final String str = (
+				args[argIndex] == null
+				? "<null>"
+				: StringUtils.toString(args[argIndex])
+			);
+			// prepare {#} tag
+			final String tagA =
+				(new StringBuilder())
+					.append('{')
+					.append(argIndex + 1)
+					.append('}')
+					.toString();
+			boolean found = false;
+			LINE_LOOP:
+			for (int lineIndex=0; lineIndex<msgs.length; lineIndex++) {
+				if (Utils.isEmpty( result[lineIndex] ))
+					continue LINE_LOOP;
+				// {#}
+				REPLACE_LOOP:
+				while (true) {
+					final int pos = result[lineIndex].indexOf(tagA);
+					if (pos == -1)
+						break REPLACE_LOOP;
+					result[lineIndex] =
+						StringUtils.ReplaceInString(
+							result[lineIndex],
+							str,
+							pos,
+							pos + tagA.length()
+						);
+					found = true;
+				} // end REPLACE_LOOP
+				if (found)
+					continue LINE_LOOP;
+			} // end LINE_LOOP
+			// append
+			if (!found)
+				extras.add(str);
+		} // end ARG_LOOP
+		// check for {} tags
+		if (extras.size() > 0) {
+			int startFromLine = 0;
+			final Iterator<String> it = extras.iterator();
+			//ARG_LOOP:
+			while (it.hasNext()) {
+				final String arg = it.next();
+				final String str = (
+					arg == null
+					? "<null>"
+					: StringUtils.toString(arg)
+				);
+				LINE_LOOP:
+				for (int lineIndex=startFromLine; lineIndex<result.length; lineIndex++) {
+					if (Utils.isEmpty( result[lineIndex] )) {
+						if (lineIndex > startFromLine)
+							startFromLine = lineIndex;
+						continue LINE_LOOP;
+					}
+					// {}
+					final int pos = result[lineIndex].indexOf("{}");
+					if (pos == -1) {
+						if (lineIndex > startFromLine)
+							startFromLine = lineIndex;
+						continue LINE_LOOP;
+					}
+					result[lineIndex] =
+						StringUtils.ReplaceInString(
+							result[lineIndex],
+							str,
+							pos,
+							pos + 2
+						);
+					it.remove();
+					break LINE_LOOP;
+				} // end LINE_LOOP
+			} // end ARG_LOOP
+		}
+		// append the rest
+		if (extras.size() > 0) {
+			final int extraSize  = extras.size();
+			final int resultSize = result.length;
+			result = Arrays.copyOf(
+				result,
+				result.length + extraSize
+			);
+			for (int index=extraSize-1; index>=0; index--) {
+				result[ resultSize + index ] = extras.get(index);
+			}
+		}
+		return result;
+	}
+
+
+
+	// replace {key} tags (in multiple lines)
+	public static String[] ReplaceTags(final String[] msgs, final Map<String, Object> args) {
+		if (Utils.isEmpty(msgs))
+			return msgs;
+		String[] result = Arrays.copyOf(msgs, msgs.length);
+		final List<String> extras = new ArrayList<String>(0);
+		//ARG_LOOP:
+		for (final String key : args.keySet()) {
+			final Object obj = args.get(key);
+			final String str = (
+				obj == null
+				? "<null>"
+				: StringUtils.toString(obj)
+			);
+			// prepare {key} tag
+			final String tagA =
+				(new StringBuilder())
+					.append('{')
+					.append(key)
+					.append('}')
+					.toString();
+			boolean found = false;
+			LINE_LOOP:
+			for (int lineIndex=0; lineIndex<msgs.length; lineIndex++) {
+				if (Utils.isEmpty( result[lineIndex] ))
+					continue LINE_LOOP;
+				// {key}
+				REPLACE_LOOP:
+				while (true) {
+					final int pos = result[lineIndex].indexOf(tagA);
+					if (pos == -1)
+						break REPLACE_LOOP;
+					result[lineIndex] =
+						StringUtils.ReplaceInString(
+							result[lineIndex],
+							str,
+							pos,
+							pos + tagA.length()
+						);
+					found = true;
+				} // end REPLACE_LOOP
+				if (found)
+					continue LINE_LOOP;
+			} // end LINE_LOOP
+			// append
+			if (!found)
+				extras.add(str);
+		} // end ARG_LOOP
+		// check for {} tags
+		if (extras.size() > 0) {
+			return
+				ReplaceTags(
+					result,
+					extras.toArray()
+				);
+		}
+		return result;
 	}
 
 
