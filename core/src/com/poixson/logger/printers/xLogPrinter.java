0ba859e65a0c51563f51fc5ce26ab9bed4696a71
lorenzop <lorenzo@poixson.com>
2018-02-02 18:33:19 -0500

    added new xLogPrinter interface

---
+++ core/src/com/poixson/logger/printers/xLogPrinter.java
@@ -0,0 +1,31 @@
+package com.poixson.logger.printers;
+
+import java.io.IOException;
+
+import com.poixson.logger.xLevel;
+import com.poixson.logger.xLogRecord;
+import com.poixson.logger.formatters.xLogFormatter;
+
+
+public interface xLogPrinter {
+
+
+	public void publish(final xLogRecord record);
+	public void publish(final String[] lines) throws IOException;
+	public void publish(final String   line ) throws IOException;
+
+
+	public void getPublishLock() throws IOException;
+	public void releasePublishLock();
+
+
+	public xLevel getLevel();
+	public void setLevel(final xLevel lvl);
+	public boolean isLoggable(final xLevel lvl);
+
+
+	public xLogFormatter getFormatter();
+	public void setFormatter(final xLogFormatter formatter);
+
+
+}
