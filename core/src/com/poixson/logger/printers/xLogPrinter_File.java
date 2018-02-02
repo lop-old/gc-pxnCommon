437a1828c532d69a243418a57e1824ef75388249
lorenzop <lorenzo@poixson.com>
2018-02-02 18:54:36 -0500

    added new xLogPrinter_File class (unfinished)

---
+++ core/src/com/poixson/logger/printers/xLogPrinter_File.java
@@ -0,0 +1,27 @@
+package com.poixson.logger.printers;
+
+
+public class xLogPrinter_File extends xLogPrinterBasic {
+
+
+
+	public xLogPrinter_File() {
+		super();
+throw new UnsupportedOperationException();
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// publish
+
+
+
+	@Override
+	public void publish(final String line) {
+//TODO:
+	}
+
+
+
+}
