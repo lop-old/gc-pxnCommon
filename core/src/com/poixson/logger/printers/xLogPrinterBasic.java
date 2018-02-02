b9dccb9faa7d2aa9e40dc854b10cc04eb3f04f23
lorenzop <lorenzo@poixson.com>
2018-02-02 18:54:17 -0500

    finished new xLogPrinter classes

--- core/src/com/poixson/logger/printers/xLogPrinterBasic.java
+++ core/src/com/poixson/logger/printers/xLogPrinterBasic.java
@@ -1,40 +1,124 @@
 package com.poixson.logger.printers;
 
-import java.lang.ref.SoftReference;
+import java.io.IOException;
+import java.util.concurrent.locks.ReentrantLock;
 
+import com.poixson.app.xVars;
 import com.poixson.logger.xLevel;
 import com.poixson.logger.xLogRecord;
+import com.poixson.logger.xLogRoot;
 import com.poixson.logger.formatters.xLogFormatter;
-import com.poixson.logger.formatters.xLogFormatter_Default;
+import com.poixson.tools.xTimeU;
 
 
-public abstract class xLogPrinter {
-
-	private volatile xLogFormatter formatter = null;
-	private volatile SoftReference<xLogFormatter> formatterDefault = null;
+public abstract class xLogPrinterBasic implements xLogPrinter {
 
 	private volatile xLevel level = null;
+	private volatile xLogFormatter formatter = null;
+
+	protected final ReentrantLock publishLock = new ReentrantLock(true);
 
 
 
-	public xLogPrinter() {
+	public xLogPrinterBasic() {
 	}
 
 
 
-	public abstract void publish(final xLogRecord record);
-	public abstract void publish(final String msg);
+	// ------------------------------------------------------------------------------- //
+	// publish
+
+
+
+	@Override
+	public void publish(final xLogRecord record) {
+		try {
+			final xLogFormatter formatter = this.getFormatter();
+			String[] lines = new String[ record.lines.length ];
+			final int linesCount = record.lines.length;
+			//LINES_LOOP:
+			for (int lineIndex=0; lineIndex<linesCount; lineIndex++) {
+				lines[ lineIndex ] =
+					formatter.formatMsg(
+						record,
+						lineIndex
+					);
+			} // end LINES_LOOP
+			this.publish( lines );
+		} catch (IOException e) {
+			e.printStackTrace(
+				xVars.getOriginalErr()
+			);
+		}
+	}
+	@Override
+	public void publish(final String[] lines) throws IOException {
+		this.getPublishLock();
+		for (final String line : lines) {
+			this.publish(line);
+		}
+		this.releasePublishLock();
+	}
+	@Override
+	public abstract void publish(final String line);
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// publish lock
+
+
+
+	@Override
+	public void getPublishLock() throws IOException {
+		this.getPublishLock(this.publishLock);
+	}
+	@Override
+	public void releasePublishLock() {
+		this.releasePublishLock(
+			this.publishLock
+		);
+	}
+
+
+
+	protected void getPublishLock(final ReentrantLock lock) throws IOException {
+		TIMEOUT_LOOP:
+		for (int i=0; i<5; i++) {
+			try {
+				if (lock.tryLock(100L, xTimeU.MS)) {
+					return;
+				}
+			} catch (InterruptedException e) {
+				throw new IOException("Failed to publish!", e);
+			}
+			if (Thread.interrupted())
+				break TIMEOUT_LOOP;
+		} // end TIMEOUT_LOOP
+		throw new IOException("Failed to publish!");
+	}
+	protected void releasePublishLock(final ReentrantLock lock) {
+		lock.unlock();
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// config
 
 
 
 	// log level
-	public void setLevel(final xLevel lvl) {
-		this.level = lvl;
-	}
+	@Override
 	public xLevel getLevel() {
 		return this.level;
 	}
+	@Override
+	public void setLevel(final xLevel lvl) {
+		this.level = lvl;
+	}
 	// is level loggable
+	@Override
 	public boolean isLoggable(final xLevel lvl) {
 		if (lvl == null || this.level == null)
 			return true;
@@ -43,36 +127,24 @@ public abstract class xLogPrinter {
 
 
 
-	protected String msgFormat(final xLogRecord record) {
-		return getFormatter()
-				.formatMsg(record);
+	// ------------------------------------------------------------------------------- //
+	// printer formatter
+
+
+
+	@Override
+	public xLogFormatter getFormatter() {
+		// existing instance
+		final xLogFormatter formatter = this.formatter;
+		if (formatter != null)
+			return formatter;
+		// default formatter
+		return xLogRoot.get()
+				.getDefaultFormatter();
 	}
-
-
-
-	// formatter
+	@Override
 	public void setFormatter(final xLogFormatter formatter) {
 		this.formatter = formatter;
-	}
-	protected xLogFormatter getFormatter() {
-		// use custom formatter
-		xLogFormatter formatter = this.formatter;
-		if (formatter != null) {
-			return formatter;
-		}
-		// use default formatter
-		return this.getDefaultFormatter();
-	}
-	public xLogFormatter getDefaultFormatter() {
-		final SoftReference<xLogFormatter> formDef = this.formatterDefault;
-		if (formDef != null) {
-			return formDef.get();
-		}
-		final xLogFormatter formatter = new xLogFormatter_Default();
-		this.formatterDefault = new SoftReference<xLogFormatter>(
-				formatter
-		);
-		return formatter;
 	}
 
 
--- core/src/com/poixson/logger/printers/xLogPrinter_stdio.java
+++ core/src/com/poixson/logger/printers/xLogPrinter_stdio.java
@@ -1,34 +1,74 @@
 package com.poixson.logger.printers;
 
-import com.poixson.logger.xLog;
-import com.poixson.logger.xLogRecord;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.ref.SoftReference;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.poixson.app.xVars;
+import com.poixson.logger.xLogRoot;
 import com.poixson.logger.console.xConsole;
 
 
-public class xLogPrinterConsole extends xLogPrinter {
+public class xLogPrinter_stdio extends xLogPrinterBasic {
+
+	protected static final ReentrantLock globalPublishLock = new ReentrantLock(true);
+
+	protected final PrintStream out;
+	protected SoftReference<xConsole> softConsole = null;
 
 
 
-	public xLogPrinterConsole() {
+	public xLogPrinter_stdio() {
 		super();
+		this.out = xVars.getOriginalOut();
+		if (this.out == null) throw new NullPointerException();
 	}
 
 
 
+	// ------------------------------------------------------------------------------- //
+	// publish
+
+
+
 	@Override
-	public void publish(final xLogRecord record) {
-		publish(
-			msgFormat(record)
+	public void publish(final String line) {
+		{
+			final SoftReference<xConsole> soft = this.softConsole;
+			xConsole console = null;
+			if (soft == null) {
+				console = xLogRoot.get().getConsole();
+				this.softConsole = new SoftReference<xConsole>( console );
+			} else {
+				console = soft.get();
+			}
+			if (console != null) {
+				console.println(line);
+				return;
+			}
+		}
+		this.out.println(line);
+	}
+
+
+
+	// ------------------------------------------------------------------------------- //
+	// publish lock
+
+
+
+	@Override
+	public void getPublishLock() throws IOException {
+		super.getPublishLock(
+			globalPublishLock
 		);
 	}
 	@Override
-	public void publish(final String msg) {
-		final xConsole console = xLog.getConsoleHandler();
-		if (console == null) {
-			System.out.println(msg);
-		} else {
-			console.print(msg);
-		}
+	public void releasePublishLock() {
+		super.releasePublishLock(
+			globalPublishLock
+		);
 	}
 
 
