4deae587c21febbbb6879e7fcf7e921cd2b41730
lorenzop <lorenzo@poixson.com>
2018-05-16 16:41:32 -0400

    finished renaming xCommand annotation

--- core/src/com/poixson/app/commands/xCommand.java
+++ core/src/com/poixson/app/commands/xCommand.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface xCommandSpec {
+public @interface xCommand {
 
 
 	String Name();
--- shell/src/com/poixson/app/commands/groups/Commands_Standard.java
+++ shell/src/com/poixson/app/commands/groups/Commands_Standard.java
@@ -1,18 +1,18 @@
 package com.poixson.app.commands.groups;
 
 import com.poixson.app.xApp;
-import com.poixson.app.commands.xCommandSpec;
+import com.poixson.app.commands.xCommand;
 
 
 public class Commands_Standard {
 
 
 
-	@xCommandSpec(Name="exit", Aliases="e,stop,quit")
+	@xCommand(Name="exit", Aliases="e,stop,quit")
 	public void __COMMAND_exit() {
 		xApp.shutdown();
 	}
-	@xCommandSpec(Name="kill", Aliases="k")
+	@xCommand(Name="kill", Aliases="k")
 	public void __COMMAND__kill() {
 		xApp.kill();
 		System.exit(1);
